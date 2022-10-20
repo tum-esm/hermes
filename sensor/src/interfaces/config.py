16ae73198e3bf1915dabed1235e1e2c144b0046b
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-20 15:04:31 +0200

    Implement sensor codebase structure (3)

---
+++ sensor/src/interfaces/config.py
@@ -0,0 +1,30 @@
+import json
+import os
+from src import types
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
+CONFIG_PATH = os.path.join(PROJECT_DIR, "config.json")
+
+
+class ConfigInterface:
+    class FileIsMissing(Exception):
+        """raised when config.json was not found"""
+
+    class FileIsInvalid(Exception):
+        """raised when config.json is not in a valid format"""
+
+    @staticmethod
+    def read() -> types.ConfigDict:
+        try:
+            with open(CONFIG_PATH, "r") as f:
+                config = json.load(f)
+            # TODO: validate structure
+        except FileNotFoundError:
+            raise ConfigInterface.FileIsMissing()
+        except json.JSONDecodeError:
+            raise ConfigInterface.FileIsInvalid("file not in a valid json format")
+        except Exception as e:
+            raise ConfigInterface.FileIsInvalid(e)
+
+        return config
--- sensor/src/types/config.py
+++ sensor/src/types/config.py
@@ -1,5 +1,10 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 
 class ConfigDict(TypedDict):
     """The config.json for each sensor"""
+
+    version: Literal["0.1.0"]
+
+
+# TODO: add function to validate a config dict
