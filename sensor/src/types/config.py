ac21f2cce22402f3463692e8b96474dd453ab805
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-25 20:02:46 +0200

    Add mqtt parameters to config

--- sensor/src/types/config.py
+++ sensor/src/types/config.py
@@ -1,14 +1,47 @@
 from typing import Literal
 import attrs
+import attrs.validators as val
 
 
 @attrs.define(frozen=True)
 class ConfigSectionGeneral:
     node_id: str = attrs.field(
         validator=[
-            attrs.validators.instance_of(str),
-            attrs.validators.min_len(3),
-            attrs.validators.max_len(128),
+            val.instance_of(str),
+            val.min_len(3),
+            val.max_len(64),
+        ]
+    )
+
+
+@attrs.define(frozen=True)
+class ConfigSectionMQTT:
+    base_topic: str = attrs.field(
+        validator=[
+            val.instance_of(str),
+            val.min_len(1),
+            val.max_len(256),
+        ]
+    )
+    url: str = attrs.field(
+        validator=[
+            val.instance_of(str),
+            val.min_len(3),
+            val.max_len(256),
+        ]
+    )
+    identifier: str = attrs.field(
+        validator=[
+            val.instance_of(str),
+            val.min_len(3),
+            val.max_len(256),
+        ]
+    )
+    password: str = attrs.field(
+        validator=[
+            val.instance_of(str),
+            val.min_len(8),
+            val.max_len(256),
         ]
     )
 
@@ -19,16 +52,32 @@ class Config:
 
     version: Literal["0.1.0"] = attrs.field(
         validator=[
-            attrs.validators.instance_of(str),
-            attrs.validators.in_(["0.1.0"]),
+            val.instance_of(str),
+            val.in_(["0.1.0"]),
         ]
     )
     general: ConfigSectionGeneral = attrs.field(
-        converter=lambda x: ConfigSectionGeneral(**x)
+        converter=lambda x: ConfigSectionGeneral(**x),
+    )
+    mqtt: ConfigSectionMQTT = attrs.field(
+        converter=lambda x: ConfigSectionMQTT(**x),
     )
 
 
 if __name__ == "__main__":
-    a = Config(**{"version": "0.1.0", "general": {"node_id": "a"}})
+    a = Config(
+        **{
+            "version": "0.1.0",
+            "general": {"node_id": "a-unique-node-id"},
+            "mqtt": {
+                "base_topic": "/.../...",
+                "url": "...",
+                "identifier": "...",
+                "password": "...",
+            },
+        }
+    )
 
-    print(a.general.node_id)
+    print(a.mqtt.identifier)
+
+    a.mqtt.identifier = "0.2.0"
