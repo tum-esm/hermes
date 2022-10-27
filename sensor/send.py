4766e7960434d8a8ba3723fcf544d8b8baaa48f8
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-27 23:07:41 +0200

    feat(sensor): implement mqtt connection (1)
    Add dummy send loop

---
+++ sensor/send.py
@@ -0,0 +1,21 @@
+import json
+import time
+from paho.mqtt.client import Client
+import ssl
+from src import interfaces
+
+config = interfaces.ConfigInterface.read()
+client = Client(client_id=config.general.node_id)
+client.username_pw_set(config.mqtt.identifier, config.mqtt.password)
+client.tls_set(certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED)
+client.connect(config.mqtt.url, port=8883, keepalive=60)
+
+i = 0
+while True:
+    client.publish(
+        config.mqtt.base_topic + "/initial-setup-test",
+        json.dumps({"type": "text", "body": f"iteration {i}"}),
+    )
+    print(f"published message {i}")
+    i += 1
+    time.sleep(2)
