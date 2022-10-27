d854121e7d8b833339d1f72e50799071a14acbbe
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-27 18:38:31 +0200

    Test default config

---
+++ sensor/tests/repository/test_default_config.py
@@ -0,0 +1,16 @@
+import json
+import os
+import sys
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
+DEFAULT_CONFIG_PATH = os.path.join(PROJECT_DIR, "config", "config.default.json")
+
+sys.path.append(PROJECT_DIR)
+from src.types import Config
+
+
+def test_default_config():
+    with open(DEFAULT_CONFIG_PATH, "r") as f:
+        default_config = json.load(f)
+    Config(**default_config)
