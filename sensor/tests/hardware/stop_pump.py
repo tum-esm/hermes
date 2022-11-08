1e5fed28b6f2dad4b9adca7644d20218d287162f
Moritz Makowski <moritz.makowski@tum.de>
2022-11-08 14:41:16 +0100

    feat(sensor): implement pump interface (9)
    debug stop_pump.py script

--- sensor/tests/hardware/stop_pump.py
+++ sensor/tests/hardware/stop_pump.py
@@ -5,9 +5,9 @@ dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
 sys.path.append(PROJECT_DIR)
 
-from src.interfaces import PumpInterface
+from src import interfaces
 
-
-pump = PumpInterface()
+config = interfaces.ConfigInterface.read()
+pump = interfaces.PumpInterface(config)
 pump.set_desired_pump_rps(0)
 pump.teardown()
