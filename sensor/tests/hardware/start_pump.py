8d36f68e0cad3ab06bd4b09b65e0ba11761dfc4a
Moritz Makowski <moritz.makowski@tum.de>
2022-11-08 12:37:31 +0100

    feat(sensor): implement valve interface (2)
    add scripts to run test cycle

---
+++ sensor/tests/hardware/start_pump.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import time
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
+sys.path.append(PROJECT_DIR)
+
+from src import interfaces
+
+
+config = interfaces.ConfigInterface.read()
+pump = interfaces.PumpInterface(config)
+pump.set_desired_pump_rps(30)
---
+++ sensor/tests/hardware/test_valve_cycle.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import time
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
+sys.path.append(PROJECT_DIR)
+
+from src import interfaces
+
+
+config = interfaces.ConfigInterface.read()
+valves = interfaces.ValveInterface(config)
+pump = interfaces.PumpInterface(config)
+pump.set_desired_pump_rps(30)
+
+for i in range(5):
+    for valve_no in range(1, 5):
+        valves.set_active_input(valve_no, logger=False)
+        time.sleep(3)
+
+pump.set_desired_pump_rps(0)
+pump.teardown()
