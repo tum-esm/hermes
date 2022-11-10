8dc3a705ce66fc209fb8464121514d0d92eeda11
Felix Böhm <felix@felixboehm.dev>
2022-11-10 22:48:30 +0100

    feat(server): add database query to insert a new configuration

--- server/app/queries/create_table_configurations.sql
+++ server/app/queries/create_table_configurations.sql
@@ -2,9 +2,11 @@ CREATE TABLE IF NOT EXISTS configurations (
     sensor_identifier TEXT NOT NULL,
     creation_timestamp INT NOT NULL,
     update_timestamp INT NOT NULL,
+    -- Add more pre-defined values here (needed if we want to visualize them in the dashboard)
+    -- Something like: lat/long, notes -> should still be nullable
     configuration JSONB NOT NULL,
     PRIMARY KEY (sensor_identifier)
 );
 
--- check if CREATE returns saying that table already exists
--- check if schema is equal, if not, error out (own .sql file as jinja template)
+-- Check if CREATE returns saying that table already exists
+-- Check if schema is equal, if not, error out (own .sql file as jinja template)
---
+++ server/app/queries/insert_configuration.sql
@@ -0,0 +1,2 @@
+INSERT INTO configurations (sensor_identifier, creation_timestamp, update_timestamp, configuration)
+VALUES ({sensor_identifier}, {creation_timestamp}, {update_timestamp}, {configuration});
