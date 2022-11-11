27775ea1a5b9821050f872f11848f6e40809a116
Felix Böhm <felix@felixboehm.dev>
2022-11-11 22:30:11 +0100

    fix(server): fix faulty validation imports

--- server/app/validation/__init__.py
+++ server/app/validation/__init__.py
@@ -1,5 +1,5 @@
 from app.validation.core import validate
-from app.validation.mqtt import MeasurementMessage
+from app.validation.mqtt import MeasurementsMessage
 from app.validation.routes import (
     GetMeasurementsRequest,
     GetSensorsRequest,
--- server/app/validation/mqtt.py
+++ server/app/validation/mqtt.py
@@ -2,7 +2,7 @@ import attrs
 
 import app.validation.constants as constants
 from app.validation.fields import (
-    JSON_FIELD_VALIDATOR,
+    JSON_VALIDATOR,
     POSITIVE_INTEGER_VALIDATOR,
     SENSOR_IDENTIFIER_VALIDATOR,
     JSONValues,
@@ -17,12 +17,12 @@ class MeasurementsMessage:
         validator=attrs.validators.deep_iterable(
             iterable_validator=attrs.validators.and_(
                 attrs.validators.instance_of(list),
-                attrs.validators.min_length(1),
-                attrs.validators.max_length(constants.Limit.MEDIUM - 1),
+                attrs.validators.min_len(1),
+                attrs.validators.max_len(constants.Limit.MEDIUM - 1),
             ),
             member_validator=attrs.validators.and_(
-                JSON_FIELD_VALIDATOR,
-                validate_contains_timestamp,
+                JSON_VALIDATOR,
+                _validate_contains_timestamp,
             ),
         )
     )
