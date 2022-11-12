a20624686f473f398713592b4cb95fd95e8ef4e2
Felix Böhm <felix@felixboehm.dev>
2022-11-13 00:20:48 +0100

    style(server): improve Jinja2 whitespace management in fetch measurements query

--- server/app/queries/fetch_measurements.sql
+++ server/app/queries/fetch_measurements.sql
@@ -6,9 +6,9 @@ FROM measurements
 {%- if request.query.sensors or request.query.start is not none or request.query.end is not none %}
 WHERE
     TRUE
-    {%- if request.query.sensors %} AND sensor_identifier = ANY({sensor_identifiers}){% endif %}
-    {%- if request.query.start is not none %} AND measurement_timestamp >= ('epoch'::TIMESTAMPTZ + {start_timestamp} * '1 second'::INTERVAL){% endif %}
-    {%- if request.query.end is not none %} AND measurement_timestamp < ('epoch'::TIMESTAMPTZ + {end_timestamp} * '1 second'::INTERVAL){% endif %}
+    {% if request.query.sensors -%} AND sensor_identifier = ANY({sensor_identifiers}) {%- endif -%}
+    {% if request.query.start is not none -%} AND measurement_timestamp >= ('epoch'::TIMESTAMPTZ + {start_timestamp} * '1 second'::INTERVAL) {%- endif -%}
+    {% if request.query.end is not none -%} AND measurement_timestamp < ('epoch'::TIMESTAMPTZ + {end_timestamp} * '1 second'::INTERVAL) {%- endif -%}
 {%- endif %}
 ORDER BY measurement_timestamp ASC
 OFFSET {skip}
