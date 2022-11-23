79253b30bd6240ef8b27e5902f14f7effffd0362
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-11-23 18:08:08 +0100

    feat(sensor): Add code structure for procedures (3)
    Describe measurement procedure

--- sensor/src/procedures/measurement.py
+++ sensor/src/procedures/measurement.py
@@ -1,1 +1,9 @@
 # doing regular measurements for a few minutes
+
+"""
+1. Check wind sensor direction every x seconds
+2. If wind direction has changes, pump according according to pipe length
+3. Check SHT 21 value every x seconds or on inlet change
+4. Apply calibration on SHT 21 value changes
+5. Do measurements
+"""
