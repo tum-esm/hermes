d258dd3f7d1d9eb8c5349a36adc8163ce308f2b5
Moritz Makowski <moritz.makowski@tum.de>
2022-11-28 22:45:50 +0100

    feat(sensor): more sensor interfaces (3)
    wait for co2 sensor startup message

--- sensor/src/interfaces/co2_sensor.py
+++ sensor/src/interfaces/co2_sensor.py
@@ -9,7 +9,9 @@ import gpiozero.pins.pigpio
 error_regex = r"OK: No errors detected\."
 
 # returned when powering up the sensor
-# TODO
+startup_regex = (
+    r"(\r\n)*GMP343 \- Version STD \d+\.\d+\r\nCopyright: Vaisala Oyj \d{4} - \d{4}(\r\n)*>"
+)
 
 # returned when calling "corr"
 # TODO
@@ -48,29 +50,31 @@ sensor_info_regex = r"\n".join(
 
 class RS232Interface:
     def __init__(self) -> None:
-        self.serial_interface = serial.Serial("/dev/ttySC0", 19200)
+        self.serial_interface = serial.Serial(
+            port="/dev/ttySC0",
+            baudrate=19200,
+            bytesize=8,
+            parity="N",
+            stopbits=1,
+        )
 
-    def write(
+    def send_command(
         self,
-        message: str,
-        sleep: float | None = None,
+        message: str
     ) -> None:
+        """send a command to the sensor. Puts a "\x1B" string before the
+        command, which will make the sensor wait until previous commands
+        have been processed"""
         self.serial_interface.write((f"\x1B {message}\r\n").encode("utf-8"))
         self.serial_interface.flush()
 
-        if sleep is not None:
-            time.sleep(sleep)
-
-    def request(self, command: str, expected_regex: str, timeout: float = 8) -> str:
-        # flush receiver stream
-        time.sleep(0.2)
+    def flush_receiver_stream(self) -> None:
+        """wait 0.2 seconds and then empty the current input queue"""
+        time.sleep(0.2)  # wait for outstanding answers from previous commands
         self.serial_interface.read_all()
 
-        # send command
-        self.write(command)
-
-        # wait for expected answer
-        expected_pattern = re.compile(f"^{expected_regex}$")
+    def wait_for_answer(self, expected_regex: str, timeout: float = 8) -> str:
+        expected_pattern = re.compile(expected_regex)
         start_time = time.time()
         answer = ""
 
@@ -110,9 +114,9 @@ class CO2SensorInterface:
         time.sleep(1)
 
         self.logger.debug("powering up sensor")
+        self.rs232_interface.flush_receiver_stream()
         self.power_pin.on()
-        time.sleep(5)
-        # TODO: wait for sensor startup message
+        self.rs232_interface.wait_for_answer(expected_regex=startup_regex)
 
         self.logger.debug("sending default settings")
         for default_setting in [
@@ -120,7 +124,7 @@ class CO2SensorInterface:
             "range 1",
             'form "Raw " CO2RAWUC " ppm; Comp." CO2RAW " ppm; Filt. " CO2 " ppm"',
         ]:
-            self.rs232_interface.write(default_setting)
+            self.rs232_interface.send_command(default_setting)
 
         # set default filters
         self.set_filter_setting()
@@ -140,10 +144,10 @@ class CO2SensorInterface:
         assert smooth >= 0 and smooth <= 255, "invalid calibration setting, smooth not in [0, 255]"
         assert median >= 0 and median <= 13, "invalid calibration setting, median not in [0, 13]"
 
-        self.rs232_interface.write(f"average {average}")
-        self.rs232_interface.write(f"smooth {smooth}")
-        self.rs232_interface.write(f"median {median}")
-        self.rs232_interface.write(f"linear {'on' if linear else 'off'}")
+        self.rs232_interface.send_command(f"average {average}")
+        self.rs232_interface.send_command(f"smooth {smooth}")
+        self.rs232_interface.send_command(f"median {median}")
+        self.rs232_interface.send_command(f"linear {'on' if linear else 'off'}")
         time.sleep(0.5)
 
         self.logger.info(
@@ -152,9 +156,10 @@ class CO2SensorInterface:
         )
 
     def get_current_concentration(self) -> types.CO2SensorData:
-        answer = self.rs232_interface.request(
-            command="send",
-            expected_regex=r"Raw\s*\d+\.\d ppm; Comp\.\s*\d+\.\d ppm; Filt\.\s*\d+\.\d ppm>?",
+        self.rs232_interface.flush_receiver_stream()
+        self.rs232_interface.send_command("send")
+        answer = self.rs232_interface.wait_for_answer(
+            expected_regex=r"^Raw\s*\d+\.\d ppm; Comp\.\s*\d+\.\d ppm; Filt\.\s*\d+\.\d ppm>?$",
         )
         for s in [" ", "Raw", "ppm", "Comp.", "Filt.", ">"]:
             answer = answer.replace(s, "")
@@ -166,15 +171,18 @@ class CO2SensorInterface:
         )
 
     def log_sensor_info(self) -> None:
-        self.rs232_interface.write("??")
+        self.rs232_interface.flush_receiver_stream()
+        self.rs232_interface.send_command("??")
         # TODO: wait for sensor answer in an expected regex
 
     def log_sensor_correction_info(self) -> None:
-        self.rs232_interface.write("corr")
+        self.rs232_interface.flush_receiver_stream()
+        self.rs232_interface.send_command("corr")
         # TODO: wait for sensor answer in an expected regex
 
     def log_sensor_errors(self) -> None:
-        self.rs232_interface.write("errs")
+        self.rs232_interface.flush_receiver_stream()
+        self.rs232_interface.send_command("errs")
         # TODO: wait for sensor answer in an expected regex
 
     def teardown(self) -> None:
