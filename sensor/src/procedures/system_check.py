ce91b54a31a63f03fa12fdc65190c07fe4af2a56
Moritz Makowski <moritz.makowski@tum.de>
2022-11-29 20:05:07 +0100

    feat(sensor): implement mainloop (2)
    add class for system checks procedure

---
+++ sensor/src/procedures/system_check.py
@@ -0,0 +1,11 @@
+from src import types
+
+
+class SystemCheckProcedure:
+    """runs every mainloop call"""
+
+    @staticmethod
+    def run(config: types.Config) -> None:
+        pass
+
+        # TODO: implement system check procedure
