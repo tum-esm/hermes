93783917b358bba42c0ed41024413e99e6530c18
Moritz Makowski <moritz.makowski@tum.de>
2022-11-29 18:06:42 +0100

    feat(sensor): more sensor interfaces (7)
    make pin factory errors more precise

--- sensor/src/utils/pin_factory.py
+++ sensor/src/utils/pin_factory.py
@@ -3,8 +3,10 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 
 def get_pin_factory() -> PiGPIOFactory:
     """return a PiGPIO pin factory (necessary for hardware PWM for pump)"""
-    pin_factory = PiGPIOFactory(host="127.0.0.1")
-    pi_error_message = 'pigpio is not connected, please run "sudo pigpiod -n 127.0.0.1"'
-    assert pin_factory.connection is not None, pi_error_message
-    assert pin_factory.connection.connected, pi_error_message
+    try:
+        pin_factory = PiGPIOFactory(host="127.0.0.1")
+        assert pin_factory.connection is not None
+        assert pin_factory.connection.connected
+    except:
+        raise ConnectionError('pigpio is not connected, please run "sudo pigpiod -n 127.0.0.1"')
     return pin_factory
