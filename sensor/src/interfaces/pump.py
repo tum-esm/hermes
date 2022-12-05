159bcfa08b2a94c3a912514c6b6fcb27796a25ec
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-05 21:16:07 +0100

    feat(sensor): refine measurement procedure (4)
    monitor pump rps in a non blocking way

--- sensor/src/interfaces/pump.py
+++ sensor/src/interfaces/pump.py
@@ -1,19 +1,20 @@
+import multiprocessing
 import queue
+import sys
 import time
 import gpiozero
 import gpiozero.pins.pigpio
 from src import utils, custom_types
 from src.utils import Constants
 
-rps_measurement_queue: queue.Queue[float] = queue.Queue()
-
 
 class PumpInterface:
     def __init__(self, config: custom_types.Config) -> None:
         self.logger = utils.Logger(config, "pump")
         self.config = config
-
         self.pin_factory = utils.gpio.get_pin_factory()
+
+        # setting desired pump speed
         self.control_pin = gpiozero.PWMOutputDevice(
             pin=Constants.Pump.control_pin_out,
             frequency=Constants.Pump.frequency,
@@ -21,44 +22,100 @@ class PumpInterface:
             initial_value=0,
             pin_factory=self.pin_factory,
         )
+
+        # measuring actual pump speed
+        self.rps_measurement_queue: queue.Queue[float] = queue.Queue()
+        self.desired_rps_queue: queue.Queue[tuple[float, float]] = queue.Queue()
         self.speed_pin = gpiozero.DigitalInputDevice(
             pin=Constants.Pump.speed_pin_in,
             pin_factory=self.pin_factory,
         )
-        self.speed_pin.when_activated = lambda: rps_measurement_queue.put(1)
+        self.speed_pin.when_activated = lambda: self.rps_measurement_queue.put(1)
+        self.rps_monitoring_process = multiprocessing.Process(
+            target=PumpInterface.monitor_rps,
+            args=(config, self.rps_measurement_queue, self.desired_rps_queue),
+        )
+        self.rps_monitoring_process.start()
 
     def set_desired_pump_rps(self, rps: float) -> None:
         """set rps between 0 and 70"""
         assert 0 <= rps <= 70, f"rps have to be between 0 and 70 (passed {rps})"
         self.control_pin.value = rps / 70
-
-    def _get_pump_cycle_count(self) -> float:
-        count = 0
-        while True:
-            try:
-                rps_measurement_queue.get_nowait()
-                count += 1
-            except queue.Empty:
-                break
-        return count / 18
-
-    def run(self, desired_rps: float, duration: float) -> None:
-        assert 2 <= desired_rps <= 70, "pump hardware limitation is 70 rps"
-        self._get_pump_cycle_count()  # empty rps_measurement_queue
-
-        self.set_desired_pump_rps(desired_rps)
-        time.sleep(duration)
-        self.set_desired_pump_rps(0)
-
-        self.logger.info(
-            f"duration = {duration}, rps = {desired_rps}, actual "
-            + f"average rps = {self._get_pump_cycle_count() / duration}"
-        )
-
-        # TODO: log warning when avg rps is differing more than 10% from the desired rps
-        # TODO: do rps monitoring without blocking
+        self.desired_rps_queue.put((time.time(), rps))
 
     def teardown(self) -> None:
         """ends all hardware/system connections"""
         self.set_desired_pump_rps(0)
+        self.rps_monitoring_process.terminate()
         self.pin_factory.close()
+
+    @staticmethod
+    def monitor_rps(
+        config: custom_types.Config,
+        rps_measurement_queue: queue.Queue[float],
+        desired_rps_queue: queue.Queue[tuple[float, float]],
+    ) -> None:
+        """monitors the rps of the pump interface. this function
+        is blocking, hence it is called in a thread"""
+
+        logger = utils.Logger(config, "pump-rps-monitoring")
+        current_rps_update_time = time.time()
+        current_rps: float = 0
+
+        while True:
+            time.sleep(10)
+
+            # expected roundes computed from the "desired rps" setting
+            # and the respective time between rps updates
+            expected_rounds: float = 0
+            try:
+                new_rps_update_time, new_rps = desired_rps_queue.get_nowait()
+                expected_rounds += (
+                    new_rps_update_time - current_rps_update_time
+                ) * current_rps
+                current_rps_update_time, current_rps = new_rps_update_time, new_rps
+            except queue.Empty:
+                break
+
+            # the 10 seconds of sleep time do not have to be exact
+            # because the latest desired-rps-block is using the exact
+            # time anyway
+            now = time.time()
+            expected_rounds += (now - current_rps_update_time) * current_rps
+            current_rps_update_time = now
+
+            # the actual rounds since the last rps-monitoring loop are
+            # counted by counting how many times the speed pin received
+            # a change from low to high since the last loop
+            actual_rounds: float = 0
+            while True:
+                try:
+                    rps_measurement_queue.get_nowait()
+                    actual_rounds += 1
+                except queue.Empty:
+                    break
+            actual_rounds /= 18
+
+            # consequences from the measurement
+            difference_in_percent = round(
+                ((actual_rounds - expected_rounds) * 100) / expected_rounds,
+                2,
+            )
+            if -7.5 < difference_in_percent < 7.5:
+                logger.debug(
+                    f"pump speed varies {difference_in_percent}%"
+                    + " target (in good range)"
+                )
+            elif -15 < difference_in_percent < 15:
+                logger.warning(
+                    f"pump speed varies {difference_in_percent}%"
+                    + " target (in noticeable range)"
+                )
+            else:
+                logger.error(
+                    f"pump speed varies {difference_in_percent}%"
+                    + " target (in unaccpetable range)"
+                )
+                sys.exit()
+                # TODO: throw exception that reaches the mainloop
+                #       possibly using thread.is_alive in a check error()
