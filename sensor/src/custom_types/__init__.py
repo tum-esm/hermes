3379d9cbd0928be974187a7a76f1a2bba69ad3fe
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-05 22:07:09 +0100

    feat(sensor): implement mqtt communication (1)
    set up pydantic types

--- sensor/src/custom_types/__init__.py
+++ sensor/src/custom_types/__init__.py
@@ -5,3 +5,4 @@ from .sensor_answers import (
     WindSensorData,
     WindSensorStatus,
 )
+from .mqtt import MQTTMessageHeader, MQTTStatusMessage, MQTTMeasurementMessage
---
+++ sensor/src/custom_types/mqtt.py
@@ -0,0 +1,31 @@
+from typing import Literal
+from pydantic import BaseModel
+from .sensor_answers import CO2SensorData
+
+
+class MQTTMessageHeader(BaseModel):
+    identifier: int
+    status: Literal["pending", "sent", "failed", "successful"]
+    revision: int
+    issue_timestamp: float
+    success_timestamp: float
+
+
+class MQTTStatusMessageBody(BaseModel):
+    severity: Literal["info", "warning", "error"]
+    subject: str
+    details: str
+
+
+class MQTTStatusMessage(BaseModel):
+    header: MQTTMessageHeader
+    body: MQTTStatusMessageBody
+
+
+class MQTTMeasurementMessageBody(BaseModel):
+    value: CO2SensorData
+
+
+class MQTTMeasurementMessage(BaseModel):
+    header: MQTTMessageHeader
+    body: MQTTMeasurementMessageBody
