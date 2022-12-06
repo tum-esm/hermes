77ab2b9f9362453c51877eb812f25ecf01c86eb5
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-06 14:57:19 +0100

    feat(sensor): implement mqtt communication (7)
    continue enqueuing methods

--- sensor/src/interfaces/mqtt_sending.py
+++ sensor/src/interfaces/mqtt_sending.py
@@ -14,23 +14,46 @@ class SendingMQTTClient:
         self.config = config
         self.logger = utils.Logger(origin="sending-mqtt-client")
 
-    # TODO: function to add status messages to queue file
-    # TODO: function to add measurement messages to queue file
     # TODO: function to pick messages from the queue file and process them
 
     def enqueue_status_message(
         self, message_body: custom_types.MQTTStatusMessageBody
     ) -> None:
-        new_identifier = 0  # TODO: determine from queue
-        new_message = custom_types.MQTTStatusMessage(
-            header=custom_types.MQTTMessageHeader(
-                identifier=new_identifier,
-                status="pending",
-                revision=self.config.revision,
-                issue_timestamp=time.time(),
-                success_timestamp=None,
-            ),
-            body=message_body,
+        self._add_message_to_queue_file(
+            custom_types.MQTTStatusMessage(
+                header=self._generate_header(),
+                body=message_body,
+            )
         )
+
+    def enqueue_measurement_message(
+        self, message_body: custom_types.MQTMeasurementMessageBody
+    ) -> None:
+        self._add_message_to_queue_file(
+            custom_types.MQTMeasurementMessage(
+                header=self._generate_header(),
+                body=message_body,
+            )
+        )
+
+    def _generate_header(self) -> custom_types.MQTTMessageHeader:
+        # TODO: determine new identifier from queue
+        new_identifier = 0
+
+        # TODO: increment new_identifier in queue file
+
+        return custom_types.MQTTMessageHeader(
+            identifier=new_identifier,
+            status="pending",
+            revision=self.config.revision,
+            issue_timestamp=time.time(),
+            success_timestamp=None,
+        )
+
+    def _add_message_to_queue_file(
+        self,
+        message: custom_types.MQTTStatusMessage | custom_types.MQTTMeasurementMessage,
+    ) -> None:
+        pass
         # TODO: add message to queue file
         # TODO: increment new_identifier in queue file
