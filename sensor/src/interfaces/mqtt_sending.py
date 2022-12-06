13b4b91918080f6cd9714b40f91a394c2fc05cb7
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-06 15:28:58 +0100

    feat(sensor): implement mqtt communication (11)
    lock communication with active queue with multiprocessing

--- sensor/src/interfaces/mqtt_sending.py
+++ sensor/src/interfaces/mqtt_sending.py
@@ -4,10 +4,13 @@ import ssl
 import time
 from paho.mqtt.client import Client
 from src import custom_types, utils
+import multiprocessing
 
 PROJECT_DIR = dirname(dirname(dirname(abspath(__file__))))
 ACTIVE_QUEUE_FILE = join(PROJECT_DIR, "data", "active-mqtt-queue.json")
 QUEUE_ARCHIVE_DIR = join(PROJECT_DIR, "data", "archive")
+
+lock = multiprocessing.Lock()
 
 
 class SendingMQTTClient:
@@ -15,44 +18,44 @@ class SendingMQTTClient:
         self.config = config
         self.logger = utils.Logger(config, origin="sending-mqtt-client")
 
-        # TODO: lock
         # generate an empty queue file if the file does not exist
-        if not isfile(ACTIVE_QUEUE_FILE):
-            self._dump_active_queue(
-                custom_types.ActiveMQTTMessageQueue(
-                    max_identifier=0,
-                    messages=[],
+        with lock:
+            if not isfile(ACTIVE_QUEUE_FILE):
+                self._dump_active_queue(
+                    custom_types.ActiveMQTTMessageQueue(
+                        max_identifier=0,
+                        messages=[],
+                    )
                 )
-            )
 
     def enqueue_message(
         self,
         message_body: custom_types.MQTTStatusMessageBody
         | custom_types.MQTTMeasurementMessageBody,
     ) -> None:
-        # TODO: lock the whole function
-        active_queue = self._load_active_queue()
-        new_header = custom_types.MQTTMessageHeader(
-            identifier=active_queue.max_identifier + 1,
-            status="pending",
-            revision=self.config.revision,
-            issue_timestamp=time.time(),
-            success_timestamp=None,
-        )
-        new_message: custom_types.MQTTStatusMessage | custom_types.MQTTMeasurementMessage
-
-        if isinstance(message_body, custom_types.MQTTStatusMessageBody):
-            new_message = custom_types.MQTTStatusMessage(
-                header=new_header, body=message_body
+        with lock:
+            active_queue = self._load_active_queue()
+            new_header = custom_types.MQTTMessageHeader(
+                identifier=active_queue.max_identifier + 1,
+                status="pending",
+                revision=self.config.revision,
+                issue_timestamp=time.time(),
+                success_timestamp=None,
             )
-        else:
-            new_message = custom_types.MQTTMeasurementMessage(
-                header=new_header, body=message_body
-            )
+            new_message: custom_types.MQTTStatusMessage | custom_types.MQTTMeasurementMessage
 
-        active_queue.messages.append(new_message)
-        active_queue.max_identifier += 1
-        self._dump_active_queue(active_queue)
+            if isinstance(message_body, custom_types.MQTTStatusMessageBody):
+                new_message = custom_types.MQTTStatusMessage(
+                    header=new_header, body=message_body
+                )
+            else:
+                new_message = custom_types.MQTTMeasurementMessage(
+                    header=new_header, body=message_body
+                )
+
+            active_queue.messages.append(new_message)
+            active_queue.max_identifier += 1
+            self._dump_active_queue(active_queue)
 
     def _load_active_queue(self) -> custom_types.ActiveMQTTMessageQueue:
         with open(ACTIVE_QUEUE_FILE, "r") as f:
