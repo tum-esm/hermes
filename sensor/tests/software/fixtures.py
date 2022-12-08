b140d5f04cb9b8e6b18cb04f3d111178b42b906a
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-08 20:49:20 +0100

    test(sensor): test mqtt communication (8)
    debug fixtures.py

--- sensor/tests/software/fixtures.py
+++ sensor/tests/software/fixtures.py
@@ -1,6 +1,6 @@
 import os
+import time
 import pytest
-import subprocess
 import dotenv
 import psutil
 from os.path import dirname, abspath, join, isfile
@@ -25,57 +25,40 @@ MOSQUITTO_PASSWORD_FILE_PATH = join(
 )
 
 
-def run_process(command: list[str]) -> None:
-    p = subprocess.run(
-        command,
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-    )
-    if p.returncode != 0:
-        raise Exception(
-            "could not generate password file: "
-            + f'stdout = "{p.stdout}", '
-            + f'stderr = "{p.stderr}"'
-        )
+def _run_process(command: str) -> None:
+    print(f'running command "{command}"')
+    exit_code = os.system(command)
+    if exit_code != 0:
+        raise Exception(f"command failed with exit code {exit_code}")
 
 
-def generate_mosquitto_config_file() -> None:
+def _generate_mosquitto_config_file() -> None:
+    print("generating mosquitto config file")
     with open(MOSQUITTO_CONFIG_PATH, "w") as f:
         f.write(
             f"allow_anonymous false\npassword_file {MOSQUITTO_PASSWORD_FILE_PATH}",
         )
 
 
-def generate_mosquitto_password_file() -> None:
-    # remove old password file
+def _generate_mosquitto_password_file() -> None:
+    print("generating mosquitto password file")
     if isfile(MOSQUITTO_PASSWORD_FILE_PATH):
         os.remove(MOSQUITTO_PASSWORD_FILE_PATH)
-    run_process(
-        [
-            "mosquitto_passwd",
-            "-b",
-            "-c",
-            MOSQUITTO_PASSWORD_FILE_PATH,
-            "test_user",
-            "test_password",
-        ]
+    _run_process(
+        f"mosquitto_passwd -b -c {MOSQUITTO_PASSWORD_FILE_PATH} "
+        + "test_user test_password",
     )
 
 
-def start_mosquitto_background_process() -> None:
-    run_process(
-        [
-            "mosquitto",
-            "-p",
-            "1883",
-            "--daemon",
-            "-c",
-            MOSQUITTO_CONFIG_PATH,
-        ]
+def _start_mosquitto_background_process() -> None:
+    print("starting mosquitto background process")
+    _run_process(
+        f"mosquitto -p 1883 --daemon -c {MOSQUITTO_CONFIG_PATH}",
     )
 
 
-def stop_mosquitto_background_process() -> None:
+def _stop_mosquitto_background_process() -> None:
+    print("stopping mosquitto background process")
     killed_process_count = 0
     for p in psutil.process_iter():
         try:
@@ -85,19 +68,27 @@ def stop_mosquitto_background_process() -> None:
                 killed_process_count += 1
         except:
             pass
-    assert killed_process_count == 1, "killed no or more than two mosquitto processes"
 
 
 @pytest.fixture(scope="session")
 def provide_mqtt_broker():
-    generate_mosquitto_config_file()
-    generate_mosquitto_password_file()
-    start_mosquitto_background_process()
+    _generate_mosquitto_config_file()
+    _generate_mosquitto_password_file()
+    _start_mosquitto_background_process()
 
     # load testing environment variables
     # used by all clients in code
     dotenv.load_dotenv(MQTT_ENV_VARS_PATH)
 
+    time.sleep(2)
+
     yield
 
-    stop_mosquitto_background_process()
+    _stop_mosquitto_background_process()
+
+
+if __name__ == "__main__":
+    _generate_mosquitto_config_file()
+    _generate_mosquitto_password_file()
+    _start_mosquitto_background_process()
+    _stop_mosquitto_background_process()
