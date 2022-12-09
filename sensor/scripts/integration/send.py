60fd927358927ef5464598252d58129a196e54db
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-09 18:09:36 +0100

    test(sensor): test mqtt communication (16)
    refactor mqtt send function

--- sensor/scripts/integration/send.py
+++ sensor/scripts/integration/send.py
@@ -1,28 +1,33 @@
 import json
 import os
+from os.path import dirname, abspath, join
 import sys
 import time
-from paho.mqtt.client import Client
-import ssl
+import dotenv
 
-dir = os.path.dirname
-PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
+PROJECT_DIR = dirname(dirname(dirname(abspath(__file__))))
+MQTT_ENV_VARS_PATH = join(PROJECT_DIR, "config", ".env.testing")
 sys.path.append(PROJECT_DIR)
 
-from src import interfaces
+from src import utils
 
-config = interfaces.ConfigInterface.read()
-client = Client(client_id="test-sender")
-client.username_pw_set(config.mqtt.identifier, config.mqtt.password)
-client.tls_set(certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED)
-client.connect(config.mqtt.url, port=config.mqtt.port, keepalive=60)
+dotenv.load_dotenv(MQTT_ENV_VARS_PATH)
+
+test_station_identifier = f"test-station-identifier-{round(time.time())}"
+test_base_topic = f"/development/{round(time.time())*2}"
+os.environ["INSERT_NAME_HERE_STATION_IDENTIFIER"] = test_station_identifier
+os.environ["INSERT_NAME_HERE_MQTT_BASE_TOPIC"] = test_base_topic
+
+mqtt_client, mqtt_config = utils.mqtt.get_mqtt_client()
+
 
 i = 0
 while True:
-    client.publish(
-        config.mqtt.base_topic + "/initial-setup-test",
+    mqtt_client.publish(
+        mqtt_config.mqtt_base_topic + "/initial-setup-test",
         json.dumps({"type": "text", "body": f"iteration {i}"}),
+        qos=1,
     )
     print(f"published message {i}")
-    i += 1
     time.sleep(2)
+    i += 1
