a7876c7490b018de1644dac3f4899184bee34606
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-13 19:53:07 +0100

    refactor(sensor): refactor mqtt client provider (1)
    add singleton MQTTClient class

--- sensor/src/utils/mqtt.py
+++ sensor/src/utils/mqtt.py
@@ -5,43 +5,91 @@ import ssl
 from src import custom_types
 
 
-def get_mqtt_config() -> custom_types.MQTTConfig:
-    return custom_types.MQTTConfig(
-        station_identifier=os.environ.get("INSERT_NAME_HERE_STATION_IDENTIFIER"),
-        mqtt_url=os.environ.get("INSERT_NAME_HERE_MQTT_URL"),
-        mqtt_port=os.environ.get("INSERT_NAME_HERE_MQTT_PORT"),
-        mqtt_username=os.environ.get("INSERT_NAME_HERE_MQTT_USERNAME"),
-        mqtt_password=os.environ.get("INSERT_NAME_HERE_MQTT_PASSWORD"),
-        mqtt_base_topic=os.environ.get("INSERT_NAME_HERE_MQTT_BASE_TOPIC"),
-    )
+class MQTTClient:
+    """provides an mqtt client"""
 
+    __config: custom_types.MQTTConfig | None = None
+    __client: paho.mqtt.client.Client | None = None
 
-def get_mqtt_client(
-    timeout_seconds: float = 5,
-) -> tuple[paho.mqtt.client.Client, custom_types.MQTTConfig]:
-    mqtt_config = get_mqtt_config()
+    @staticmethod
+    def init(timeout_seconds: float = 5) -> None:
+        """v and
+        initializes the mqtt client. timeout_seconds is the amount
+        of time the mqtt connection process is allowed to take."""
+        MQTTClient.__init_config()
+        MQTTClient.__init_client(timeout_seconds=timeout_seconds)
 
-    mqtt_client = paho.mqtt.client.Client(client_id=mqtt_config.station_identifier)
-    mqtt_client.username_pw_set(mqtt_config.mqtt_username, mqtt_config.mqtt_password)
-    mqtt_client.tls_set(
-        certfile=None,
-        keyfile=None,
-        cert_reqs=ssl.CERT_REQUIRED,
-        tls_version=ssl.PROTOCOL_TLS_CLIENT,
-    )
-    mqtt_client.connect(
-        mqtt_config.mqtt_url, port=int(mqtt_config.mqtt_port), keepalive=60
-    )
-    mqtt_client.loop_start()
+    @staticmethod
+    def get_config() -> custom_types.MQTTConfig:
+        """returns the mqtt config, requires init() to be run before"""
+        assert (
+            MQTTClient.__config is not None
+        ), "please run init() before requesting the config"
+        return MQTTClient.__config
 
-    start_time = time.time()
-    while True:
-        if mqtt_client.is_connected():
-            break
-        if (time.time() - start_time) > timeout_seconds:
-            raise TimeoutError(
-                f"mqtt client is not connected (using params {mqtt_config})"
-            )
-        time.sleep(0.1)
+    @staticmethod
+    def get_client() -> paho.mqtt.client.Client:
+        """returns the mqtt client, requires init() to be run before"""
+        assert (
+            MQTTClient.__client is not None
+        ), "please run init() before requesting the client"
+        return MQTTClient.__client
 
-    return mqtt_client, mqtt_config
+    @staticmethod
+    def deinit() -> None:
+        """disconnected the mqtt client and removes the internal class state"""
+        assert (
+            MQTTClient.__client is not None
+        ), "please run init() before requesting the client"
+        MQTTClient.__client.disconnect()
+        MQTTClient.__client = None
+        MQTTClient.__config = None
+
+    @staticmethod
+    def __init_config() -> None:
+        """loads the mqtt config from environment variables"""
+        MQTTClient.__config = custom_types.MQTTConfig(
+            station_identifier=os.environ.get("INSERT_NAME_HERE_STATION_IDENTIFIER"),
+            mqtt_url=os.environ.get("INSERT_NAME_HERE_MQTT_URL"),
+            mqtt_port=os.environ.get("INSERT_NAME_HERE_MQTT_PORT"),
+            mqtt_username=os.environ.get("INSERT_NAME_HERE_MQTT_USERNAME"),
+            mqtt_password=os.environ.get("INSERT_NAME_HERE_MQTT_PASSWORD"),
+            mqtt_base_topic=os.environ.get("INSERT_NAME_HERE_MQTT_BASE_TOPIC"),
+        )
+
+    @staticmethod
+    def __init_client(timeout_seconds: float = 5) -> None:
+        """initializes the mqtt client. timeout_seconds is the amount
+        of time the mqtt connection process is allowed to take."""
+        assert MQTTClient.__config is not None
+
+        mqtt_client = paho.mqtt.client.Client(
+            client_id=MQTTClient.__config.station_identifier
+        )
+        mqtt_client.username_pw_set(
+            MQTTClient.__config.mqtt_username, MQTTClient.__config.mqtt_password
+        )
+        mqtt_client.tls_set(
+            certfile=None,
+            keyfile=None,
+            cert_reqs=ssl.CERT_REQUIRED,
+            tls_version=ssl.PROTOCOL_TLS_CLIENT,
+        )
+        mqtt_client.connect(
+            MQTTClient.__config.mqtt_url,
+            port=int(MQTTClient.__config.mqtt_port),
+            keepalive=60,
+        )
+        mqtt_client.loop_start()
+
+        start_time = time.time()
+        while True:
+            if mqtt_client.is_connected():
+                break
+            if (time.time() - start_time) > timeout_seconds:
+                raise TimeoutError(
+                    f"mqtt client is not connected (using params {MQTTClient.config})"
+                )
+            time.sleep(0.1)
+
+        MQTTClient.__client = mqtt_client
