4fbc16c835f5b8b3eeb68089d4858e54b88e0b66
Moritz Makowski <moritz.makowski@tum.de>
2022-12-15 16:01:38 +0100

    refactor(sensor): refactor air inlet sensor test

---
+++ sensor/tests/hardware/test_air_inlet_sensor.py
@@ -0,0 +1,16 @@
+from os.path import dirname, abspath
+import sys
+import pytest
+from ..pytest_fixtures import log_files
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.append(PROJECT_DIR)
+from src import hardware_interfaces
+
+
+@pytest.mark.integration
+def test_air_inlet_sensor(log_files: None) -> None:
+    sensor = hardware_interfaces.AirInletSensorInterface()
+    temperature, humidity = sensor.get_current_values()
+    assert temperature is not None
+    assert humidity is not None
