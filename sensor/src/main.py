69e8549f8823405fddf9787b05d6368c8048bb0a
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-15 01:59:01 +0100

    feat(sensor): add try-except to mainloop

--- sensor/src/main.py
+++ sensor/src/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from src import procedures, utils
 
 
@@ -41,23 +42,34 @@ def run() -> None:
     system_check_prodecure = procedures.SystemCheckProcedure(config)
     measurement_prodecure = procedures.MeasurementProcedure(config)
 
-    # TODO: add try-except logic around mainloop
-    # TODO: send exceptions over mqtt as well
-    # TODO: add incremental backoff time
+    backoff_time_bucket_index = 0
+    backoff_time_buckets = [15, 60, 300, 900]
 
     while True:
-        logger.info("starting mainloop iteration")
+        try:
+            logger.info("starting mainloop iteration")
 
-        logger.info("running system checks")
-        system_check_prodecure.run()
+            logger.info("running system checks")
+            system_check_prodecure.run()
 
-        # TODO: read mqtt messages
-        # TODO: optionally call configuration routine -> triggers a restart if config is accepted
-        # TODO: optionally call calibration routing
-        # TODO: run teardown of other procedures before configuration/calibration
+            # TODO: read mqtt messages
+            # TODO: optionally call configuration routine -> triggers a restart if config is accepted
+            # TODO: optionally call calibration routing
+            # TODO: run teardown of other procedures before configuration/calibration
 
-        # TODO: if messages are empty, run, skip otherwise
-        logger.info("running measurements")
-        measurement_prodecure.run()
+            # TODO: if messages are empty, run, skip otherwise
+            logger.info("running measurements")
+            measurement_prodecure.run()
 
-        logger.info("finished mainloop iteration")
+            logger.info("finished mainloop iteration")
+            backoff_time_bucket_index = 0
+
+        except Exception as e:
+            logger.exception(e, config=config)
+
+            # wait for an increasing amount of time
+            current_backoff_time = backoff_time_buckets[backoff_time_bucket_index]
+            logger.error(f"waiting for {current_backoff_time} seconds", config=config)
+            time.sleep(current_backoff_time)
+            if len(backoff_time_buckets) > (backoff_time_bucket_index + 1):
+                backoff_time_bucket_index += 1
