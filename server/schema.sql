814dc13c8b2ff05096a8a2b33219489dca57454a
Felix Böhm <felix@felixboehm.dev>
2023-01-12 20:52:29 +0100

    fix(server): Fix faulty SQL extension creation syntax

--- server/schema.sql
+++ server/schema.sql
@@ -1,5 +1,5 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" CASCADE;
-CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "timescaledb";
 
 
 -- Function to convert TIMESTAMPTZ into unix timestamp
