bcf0a5d37308bce788a8fd57c730576b6dce67c4
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-13 13:09:24 +0100

    feat(sensor): Implement heated enclosure code (3)
    make daniel's script compile and upload

--- sensor/cli/commands/enclosure_arduino/compile_and_upload.py
+++ sensor/cli/commands/enclosure_arduino/compile_and_upload.py
@@ -1,7 +1,6 @@
-import json
 import os
 import click
-import utils, custom_types
+import utils
 
 dirname = os.path.dirname
 PROJECT_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
@@ -14,6 +13,8 @@ def compile_and_upload() -> None:
         f"arduino-cli compile --verbose "
         + f"--fqbn arduino:avr:nano:cpu=atmega328old "
         + f"--output-dir {ARDUINO_SCRIPT_PATH} "
+        + f"--library {os.path.join(ARDUINO_SCRIPT_PATH, 'OneWire-2.3.7')} "
+        + f"--library {os.path.join(ARDUINO_SCRIPT_PATH, 'DallasTemperature-3.9.0')} "
         + f"{ARDUINO_SCRIPT_PATH}"
     )
 
