59ad37236bebaa39835ccf970dc54714aa154dcb
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-13 18:31:06 +0100

    feat(sensor): Add empty run_headless_enclosure.py

---
+++ sensor/run_headless_enclosure.py
@@ -0,0 +1,10 @@
+from src import main
+
+
+if __name__ == "__main__":
+    pass
+
+    # TODO: init config interface
+    # TODO: init heated enclosure interface
+    # TODO: run heater enclosure interface
+    # TODO: record measurement activity every 10 seconds in a dedicated file
--- sensor/scripts/testing/check_static_types.sh
+++ sensor/scripts/testing/check_static_types.sh
@@ -3,6 +3,9 @@ set -o errexit
 echo "Checking automation types"
 mypy run.py
 
+echo "Checking headless enclosure types"
+mypy run_headless_enclosure.py
+
 echo "Checking CLI types"
 mypy cli/main.py
 
