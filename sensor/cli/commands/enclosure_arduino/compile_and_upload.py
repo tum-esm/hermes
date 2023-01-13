acfca8d1df84ea0ed0cd1f1d334e62c3f7de5933
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-13 14:03:13 +0100

    feat(sensor): Implement heated enclosure code (7)
    pass enclosure config from CLI

--- sensor/cli/commands/enclosure_arduino/compile_and_upload.py
+++ sensor/cli/commands/enclosure_arduino/compile_and_upload.py
@@ -6,9 +6,28 @@ dirname = os.path.dirname
 PROJECT_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
 ARDUINO_SCRIPT_PATH = os.path.join(PROJECT_DIR, "src", "heated_enclosure")
 
+ARDUINO_CONFIG_TEMPLATE_PATH = os.path.join(ARDUINO_SCRIPT_PATH, "config.template.h")
+ARDUINO_CONFIG_PATH = os.path.join(ARDUINO_SCRIPT_PATH, "config.h")
+
 
 @click.command(help="List all connected arduino boards")
 def compile_and_upload() -> None:
+
+    with open(ARDUINO_CONFIG_TEMPLATE_PATH) as f:
+        config_content = f.read()
+
+    # TODO: use version from pyproject.toml
+    # TODO: use params from config
+    for k, v in {
+        "CODEBASE_VERSION": '"0.1.0"',
+        "TARGET_TEMPERATURE": "40",
+        "ALLOWED_TEMPERATURE_DEVIATION": "3",
+    }.items():
+        config_content = config_content.replace(f"%{k}%", v)
+
+    with open(ARDUINO_CONFIG_PATH, "w") as f:
+        f.write(config_content)
+
     utils.run_shell_command(
         f"arduino-cli compile --verbose "
         + f"--fqbn arduino:avr:nano:cpu=atmega328old "
