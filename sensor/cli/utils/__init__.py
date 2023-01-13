99ee8af24ff45efdcc30ce96ff1c26f61042b1c6
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-13 17:51:45 +0100

    feat(sensor): Debug CLI

--- sensor/cli/utils/__init__.py
+++ sensor/cli/utils/__init__.py
@@ -1,3 +1,8 @@
-from .determine_process_status import terminate_processes, process_is_running
+from .determine_process_status import (
+    terminate_processes,
+    process_is_running,
+    INTERPRETER_PATH,
+    SCRIPT_PATH,
+)
 from .print_colors import print_green, print_red
 from .run_shell_command import run_shell_command
