7ae4944439a1a838a953f10c9545c78c17a98779
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-15 20:08:26 +0100

    bug(sensor): fix mypy stuff

--- sensor/tests/hardware/test_mainboard_sensor.py
+++ sensor/tests/hardware/test_mainboard_sensor.py
@@ -5,10 +5,11 @@ from ..pytest_fixtures import log_files
 
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 sys.path.append(PROJECT_DIR)
-from src import hardware_interfaces
+from src import hardware_interfaces, utils
 
 
 @pytest.mark.integration
 def test_mainboard_sensor(log_files: None) -> None:
-    sensor = hardware_interfaces.MainboardSensorInterface()
+    config = utils.ConfigInterface.read()
+    sensor = hardware_interfaces.MainboardSensorInterface(config)
     sensor.get_system_data()
