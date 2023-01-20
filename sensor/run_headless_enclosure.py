5149b1c84b7f90813809b4501f869b8543349e21
Moritz Makowski <moritz.makowski@tum.de>
2023-01-20 16:04:33 +0100

    feat(sensor): debug heated enclosure code (4)

--- sensor/run_headless_enclosure.py
+++ sensor/run_headless_enclosure.py
@@ -2,9 +2,12 @@ import json
 import os
 import time
 from src import utils, hardware, custom_types
+import filelock
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DST_FILE_PATH = os.path.join(PROJECT_DIR, "logs", "headless-enclosure-data.log")
+LOCK_FILE_PATH = os.path.join(PROJECT_DIR, "logs", "headless-enclosure-data.lock")
+lock = filelock.FileLock(LOCK_FILE_PATH, timeout=2)
 
 
 def write_data(data: custom_types.HeatedEnclosureData) -> None:
@@ -13,22 +16,27 @@ def write_data(data: custom_types.HeatedEnclosureData) -> None:
 
 
 if __name__ == "__main__":
-    config = utils.ConfigInterface.read()
-    heated_enclosure = hardware.HeatedEnclosureInterface(config)
+    try:
+        with lock:
+            print(f"acquired lock with PID {os.getpid()}")
+            config = utils.ConfigInterface.read()
+            heated_enclosure = hardware.HeatedEnclosureInterface(config)
 
-    print("sleeping 10 seconds to wait for data")
-    time.sleep(10)
+            print("sleeping 10 seconds to wait for data")
+            time.sleep(10)
 
-    last_update_time = None
+            last_update_time = None
 
-    while True:
-        current_data = heated_enclosure.get_current_data()
-        assert current_data is not None, "enclosure doesn't send any data"
+            while True:
+                current_data = heated_enclosure.get_current_data()
+                assert current_data is not None, "enclosure doesn't send any data"
 
-        if (last_update_time is None) or (
-            last_update_time != current_data.last_update_time
-        ):
-            write_data(current_data)
-            last_update_time = current_data.last_update_time
+                if (last_update_time is None) or (
+                    last_update_time != current_data.last_update_time
+                ):
+                    write_data(current_data)
+                    last_update_time = current_data.last_update_time
 
-        time.sleep(1)
+                time.sleep(1)
+    except filelock.Timeout:
+        print("could not acquire lock")
