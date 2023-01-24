f3e4d4adcbbd8b46363123cf66a380e462b08cd1
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-24 14:25:05 +0100

    feat(sensor): refine headless enclosure script

--- sensor/run_headless_enclosure.py
+++ sensor/run_headless_enclosure.py
@@ -12,9 +12,16 @@ LOCK_FILE_PATH = os.path.join(PROJECT_DIR, "logs", "headless-enclosure-data.lock
 lock = filelock.FileLock(LOCK_FILE_PATH, timeout=2)
 
 
-def write_data(data: custom_types.HeatedEnclosureData) -> None:
+def write_data(
+    enclosure_data: custom_types.HeatedEnclosureData,
+    mainboard_sensor_data: custom_types.MainboardSensorData,
+) -> None:
     with open(DST_FILE_PATH, "a") as f:
-        f.write(f"{json.dumps(data.dict())}\n")
+        merged_dict = {
+            **enclosure_data.dict(),
+            **mainboard_sensor_data.dict(),
+        }
+        f.write(f"{json.dumps(merged_dict)}\n")
 
 
 if __name__ == "__main__":
@@ -23,6 +30,7 @@ if __name__ == "__main__":
             print(f"acquired lock with PID {os.getpid()} at {datetime.now()}")
             config = utils.ConfigInterface.read()
             heated_enclosure = hardware.HeatedEnclosureInterface(config)
+            mainboard_sensor = hardware.MainboardSensorInterface(config)
 
             print("sleeping 10 seconds to wait for data")
             time.sleep(10)
@@ -34,7 +42,7 @@ if __name__ == "__main__":
                 assert current_data is not None, "enclosure doesn't send any data"
 
                 if last_update_time != current_data.last_update_time:
-                    write_data(current_data)
+                    write_data(current_data, mainboard_sensor.get_system_data())
                     last_update_time = current_data.last_update_time
 
                 # cycle power on USB ports if Arduino hast answered for 2 minutes
