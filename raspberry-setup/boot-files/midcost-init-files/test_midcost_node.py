69dca3c702140575629a41702b16f9f9a93be8aa
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-25 20:06:53 +0100

    feat(sensor): add initialization script (11)
    implement test script

--- raspberry-setup/boot-files/midcost-init-files/test_midcost_node.py
+++ raspberry-setup/boot-files/midcost-init-files/test_midcost_node.py
@@ -1,2 +1,17 @@
-# TODO: test baserow ip logger
-# TODO: test insert-name-here
+from utils import run_shell_command, IP_LOGGER_DIR, AUTOMATION_DIR, AUTOMATION_TAG
+
+# =============================================================================
+# TEST BASEROW-IP-LOGGER
+
+run_shell_command(
+    '.venv/bn/python -m pytest -m "integration"',
+    working_directory=f"{IP_LOGGER_DIR}",
+)
+
+# =============================================================================
+# TEST INSERT-NAME-HERE
+
+run_shell_command(
+    '.venv/bn/python -m pytest -m "integration"',
+    working_directory=f"{AUTOMATION_DIR}/{AUTOMATION_TAG}",
+)
