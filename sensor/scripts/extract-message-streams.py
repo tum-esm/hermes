ae370fd5106836b760dfab767ed3e5690510af86
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-02 16:05:50 +0100

    feat(sensor): extract messages from stream (3)
    add plots

--- sensor/scripts/extract-message-streams.py
+++ sensor/scripts/extract-message-streams.py
@@ -52,7 +52,7 @@ air_data = [
 system_data = [
     (datetime.fromtimestamp(m.body.timestamp), m.body.value)
     for m in data_messages
-    if isinstance(m.body.value, custom_types.MQTTCO2Data)
+    if isinstance(m.body.value, custom_types.MQTTSystemData)
 ]
 
 print(f"len(co2_data) = {len(co2_data)}")
@@ -76,22 +76,52 @@ top_plot.scatter(
     label="raw co2",
     color="red",
     s=0.05,
-    alpha=0.25,
+    alpha=0.5,
 )
-"""
-top_plot.scatter(
-    [c[0] for c in co2_data],
-    [c[1].data.compensated for c in co2_data],
-    label="compensated co2",
-    color="blue",
-    s=0.05,
-    alpha=0.25,
-)"""
 
 top_plot.xaxis.set_minor_locator(dates.SecondLocator(interval=4 * 3600))  # every hours
 top_plot.xaxis.set_minor_formatter(dates.DateFormatter("%H:%M"))  # hours and minutes
 top_plot.xaxis.set_major_locator(dates.DayLocator(interval=1))  # every day
 top_plot.xaxis.set_major_formatter(dates.DateFormatter("\n%d-%m-%Y"))
+
+bottom_plot = plt.subplot(2, 1, 2)
+bottom_plot.grid(True)
+bottom_plot.set_title("Temperature Data")
+bottom_plot.set_xlabel("local time")
+bottom_plot.set_ylabel("")
+
+bottom_plot.scatter(
+    [c[0] for c in air_data],
+    [c[1].data.inlet_temperature for c in air_data],
+    label="inlet temperature",
+    color="red",
+    s=0.2,
+    alpha=0.75,
+)
+bottom_plot.scatter(
+    [c[0] for c in system_data],
+    [c[1].data.cpu_temperature for c in system_data],
+    label="cpu temperature",
+    color="green",
+    s=0.2,
+    alpha=0.75,
+)
+bottom_plot.scatter(
+    [c[0] for c in system_data],
+    [c[1].data.mainboard_temperature for c in system_data],
+    label="mainboard temperature",
+    color="blue",
+    s=0.2,
+    alpha=0.75,
+)
+bottom_plot.legend()
+
+bottom_plot.xaxis.set_minor_locator(
+    dates.SecondLocator(interval=4 * 3600)
+)  # every hours
+bottom_plot.xaxis.set_minor_formatter(dates.DateFormatter("%H:%M"))  # hours and minutes
+bottom_plot.xaxis.set_major_locator(dates.DayLocator(interval=1))  # every day
+bottom_plot.xaxis.set_major_formatter(dates.DateFormatter("\n%d-%m-%Y"))
 
 plt.savefig("some.png", dpi=300)
 
