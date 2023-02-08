b746895046c9d8793055ce90d285ef53e3c99164
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-08 17:00:37 +0100

    fix(sensor): globalize semaphore for run_automation.py

--- sensor/run_automation.py
+++ sensor/run_automation.py
@@ -1,16 +1,15 @@
 import dotenv
 import os
+from os.path import dirname
 import filelock
 from src import main
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = dirname(dirname(os.path.abspath(__file__)))
+PROJECT_DIR = dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     dotenv.load_dotenv(os.path.join(PROJECT_DIR, "config", ".env"))
-    lock = filelock.FileLock(
-        os.path.join(PROJECT_DIR, "run_automation.lock"),
-        timeout=2,
-    )
+    lock = filelock.FileLock(os.path.join(ROOT_DIR, "run_automation.lock"), timeout=2)
 
     try:
         with lock:
