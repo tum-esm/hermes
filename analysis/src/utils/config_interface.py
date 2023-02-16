5f8cd082ae25c2932f774bc03d4953964a5b382e
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-16 18:54:10 +0100

    feat(analysis): add config interface

---
+++ analysis/src/utils/config_interface.py
@@ -0,0 +1,27 @@
+import json
+import os
+from os.path import dirname
+from src import custom_types
+
+PROJECT_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
+CONFIG_PATH = os.path.join(PROJECT_DIR, "config", "config.json")
+
+
+class ConfigInterface:
+    class FileIsMissing(Exception):
+        """raised when config.json was not found"""
+
+    class FileIsInvalid(Exception):
+        """raised when config.json is not in a valid format"""
+
+    @staticmethod
+    def read() -> custom_types.Config:
+        try:
+            with open(CONFIG_PATH, "r") as f:
+                return custom_types.Config(**json.load(f))
+        except FileNotFoundError:
+            raise ConfigInterface.FileIsMissing()
+        except json.JSONDecodeError:
+            raise ConfigInterface.FileIsInvalid("file not in a valid json format")
+        except Exception as e:
+            raise ConfigInterface.FileIsInvalid(e.args[0])
