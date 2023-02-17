af9e01711dcc1fbdf9b3cd57a34d9dec9d286fa5
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-17 23:15:05 +0100

    refactor(analysis): plot sensor activity (8)
    make plots more amaßing

--- analysis/plot_sensor_activity.py
+++ analysis/plot_sensor_activity.py
@@ -1,4 +1,5 @@
 import colorsys
+from datetime import timedelta
 import math
 from matplotlib import pyplot as plt
 import polars as pl
@@ -15,7 +16,6 @@ LOGS_DF_CACHE_PATH = os.path.join(PROJECT_DIR, "cache", "grouped_logs_df.parquet
 
 MEASUREMENT_TYPE = [
     "co2",
-    "calibration",
     "air",
     "system",
     "wind",
@@ -24,23 +24,27 @@ MEASUREMENT_TYPE = [
 LOG_TYPE = ["info", "warning", "error"]
 
 DATA_TYPE_COLOR = {
-    t: "#"
-    + "".join(
-        [
-            hex(int(c * 255))[2:].zfill(2)
-            for c in colorsys.hls_to_rgb((i * 12 / 360), 0.7, 0.8)
-        ]
-    )
-    for i, t in enumerate(MEASUREMENT_TYPE + LOG_TYPE)
+    "co2": "#ef4444",  # red-500
+    "air": "#f97316",  # orange-500
+    "system": "#22c55e",  # green-500
+    "wind": "#0f766e",  # teal-500
+    "enclosure": "#1d4ed8",  # blue-500
+    "info": "#22c55e",  # green-500
+    "warning": "#f97316",  # orange-500
+    "error": "#ef4444",  # red-500
 }
-DATA_TYPE_OFFSET = {
-    t: -round((i * 0.05) + (0.05 * math.floor(i / 5)), 2)
-    for i, t in enumerate(MEASUREMENT_TYPE + LOG_TYPE)
+MEASUREMENT_TYPE_OFFSET = {
+    "co2": 0,
+    "air": 0.045,
+    "system": 0.015,
+    "wind": -0.015,
+    "enclosure": -0.045,
 }
+LOG_TYPE_OFFSET = {t: -round(i * 0.03, 2) for i, t in enumerate(LOG_TYPE)}
 
 if __name__ == "__main__":
     config = utils.ConfigInterface.read()
-    sensor_number = 20
+    sensor_number = 3
     sensor_name = f"tum-esm-midcost-raspi-{sensor_number}"
 
     if (not os.path.exists(MEASUREMENT_DF_CACHE_PATH)) or (
@@ -98,34 +102,70 @@ if __name__ == "__main__":
     print(grouped_measurements_df)
     print(grouped_logs_df)
 
+    grouped_measurements_df = grouped_measurements_df.groupby_rolling(
+        "timestamp", period=timedelta(minutes=30)
+    ).agg([pl.col(f"{t}_rpm").mean() for t in MEASUREMENT_TYPE])
+
+    # grouped_logs_df = grouped_logs_df.groupby_rolling(
+    #    "timestamp", period=timedelta(minutes=5)
+    # )
+
     plt.subplots(
+        3,
         1,
-        1,
-        gridspec_kw={"height_ratios": [2], "hspace": 0.5},
-        figsize=(12, 6),
+        gridspec_kw={"height_ratios": [2, 2, 2], "hspace": 1},
+        figsize=(12, 8),
     )
 
     with utils.plot(
-        subplot_row_count=1,
+        subplot_row_count=3,
         subplot_col_count=1,
         subplot_number=1,
         xlabel="UTC time",
         ylabel="code version with\nactive measurement data",
-        title="Used code version over time",
+        title="CO2 Messages",
+        xaxis_scale="days",
+    ) as p:
+        xs = grouped_measurements_df.get_column("timestamp")
+        ys = [
+            y + MEASUREMENT_TYPE_OFFSET["co2"]
+            for y in grouped_measurements_df.get_column(f"co2_rpm")
+        ]
+        p.plot(
+            xs, ys, linewidth=1.5, color=DATA_TYPE_COLOR["co2"], alpha=1, label="co2"
+        )
+
+    with utils.plot(
+        subplot_row_count=3,
+        subplot_col_count=1,
+        subplot_number=2,
+        xlabel="UTC time",
+        ylabel="code version with\nactive measurement data",
+        title="Other Measurement Messages",
         xaxis_scale="days",
     ) as p:
         for t in MEASUREMENT_TYPE:
+            if t == "co2":
+                continue
             xs = grouped_measurements_df.get_column("timestamp")
             ys = [
-                y + DATA_TYPE_OFFSET[t]
+                y + MEASUREMENT_TYPE_OFFSET[t]
                 for y in grouped_measurements_df.get_column(f"{t}_rpm")
             ]
-            p.scatter(xs, ys, s=1, color=DATA_TYPE_COLOR[t], alpha=0.5)
+            p.plot(xs, ys, linewidth=1.5, color=DATA_TYPE_COLOR[t], alpha=1, label=t)
+
+    with utils.plot(
+        subplot_row_count=3,
+        subplot_col_count=1,
+        subplot_number=3,
+        xlabel="UTC time",
+        ylabel="code version with\nactive measurement data",
+        title="Log Messages",
+        xaxis_scale="days",
+    ) as p:
         for t in LOG_TYPE:
             xs = grouped_logs_df.get_column("timestamp")
-            ys = [
-                y + DATA_TYPE_OFFSET[t] for y in grouped_logs_df.get_column(f"{t}_rpm")
-            ]
-            p.scatter(xs, ys, s=1.2, color=DATA_TYPE_COLOR[t], alpha=0.8)
+            ys = [LOG_TYPE_OFFSET[t] for y in grouped_logs_df.get_column(f"{t}_rpm")]
+            p.scatter(xs, ys, s=2.5, color=DATA_TYPE_COLOR[t], alpha=1, label=t)
 
     utils.save_plot(f"sensor_activity_{sensor_number}.png")
