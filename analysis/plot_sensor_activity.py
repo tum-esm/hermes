f2b531601c591107ea13465ea9d8ff33273433f6
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-17 22:43:58 +0100

    refactor(analysis): plot sensor activity (7)
    add cache, plot colors and offsets

--- analysis/plot_sensor_activity.py
+++ analysis/plot_sensor_activity.py
@@ -1,59 +1,102 @@
+import colorsys
+import math
 from matplotlib import pyplot as plt
 import polars as pl
+from os.path import dirname
+import os
 from src import utils
+
+PROJECT_DIR = dirname(os.path.abspath(__file__))
+MEASUREMENT_DF_CACHE_PATH = os.path.join(
+    PROJECT_DIR, "cache", "grouped_measurements_df.parquet"
+)
+LOGS_DF_CACHE_PATH = os.path.join(PROJECT_DIR, "cache", "grouped_logs_df.parquet")
+
+
+MEASUREMENT_TYPE = [
+    "co2",
+    "calibration",
+    "air",
+    "system",
+    "wind",
+    "enclosure",
+]
+LOG_TYPE = ["info", "warning", "error"]
+
+DATA_TYPE_COLOR = {
+    t: "#"
+    + "".join(
+        [
+            hex(int(c * 255))[2:].zfill(2)
+            for c in colorsys.hls_to_rgb((i * 12 / 360), 0.7, 0.8)
+        ]
+    )
+    for i, t in enumerate(MEASUREMENT_TYPE + LOG_TYPE)
+}
+DATA_TYPE_OFFSET = {
+    t: -round((i * 0.05) + (0.05 * math.floor(i / 5)), 2)
+    for i, t in enumerate(MEASUREMENT_TYPE + LOG_TYPE)
+}
 
 if __name__ == "__main__":
     config = utils.ConfigInterface.read()
     sensor_number = 20
     sensor_name = f"tum-esm-midcost-raspi-{sensor_number}"
 
-    measurements = utils.SQLQueries.fetch_sensor_measurements(config, sensor_name)
-    logs = utils.SQLQueries.fetch_sensor_logs(config, sensor_name)
+    if (not os.path.exists(MEASUREMENT_DF_CACHE_PATH)) or (
+        not os.path.exists(LOGS_DF_CACHE_PATH)
+    ):
+        measurements = utils.SQLQueries.fetch_sensor_measurements(config, sensor_name)
+        logs = utils.SQLQueries.fetch_sensor_logs(config, sensor_name)
 
-    print(f"plotting activity of {sensor_name}")
-    print(f"{len(measurements)} measurement(s)")
-    print(f"{len(logs)} log(s)")
+        print(f"plotting activity of {sensor_name}")
+        print(f"{len(measurements)} measurement(s)")
+        print(f"{len(logs)} log(s)")
 
-    measurements_df = pl.DataFrame(
-        {
-            "timestamp": [m.timestamp for m in measurements],
-            "variant": [m.value.variant for m in measurements],
-        }
-    ).sort(by="timestamp")
-    logs_df = pl.DataFrame(
-        {
-            "timestamp": [l.timestamp for l in logs],
-            "severity": [l.severity for l in logs],
-        }
-    ).sort(by="timestamp")
+        measurements_df = pl.DataFrame(
+            {
+                "timestamp": [m.timestamp for m in measurements],
+                "variant": [m.value.variant for m in measurements],
+            }
+        ).sort(by="timestamp")
+        logs_df = pl.DataFrame(
+            {
+                "timestamp": [l.timestamp for l in logs],
+                "severity": [l.severity for l in logs],
+            }
+        ).sort(by="timestamp")
 
-    # print(measurements_df)
-    # print(logs_df)
+        # print(measurements_df)
+        # print(logs_df)
 
-    measurement_data_type = ["co2", "calibration", "air", "system", "wind", "enclosure"]
-    log_data_type = ["info", "warning", "error"]
+        grouped_measurements_df = measurements_df.groupby_dynamic(
+            "timestamp", every="2m"
+        ).agg(
+            [
+                ((pl.col("variant").filter(pl.col("variant") == t)).count()).alias(
+                    f"{t}_rpm"
+                )
+                for t in MEASUREMENT_TYPE
+            ]
+        )
+        grouped_logs_df = logs_df.groupby_dynamic("timestamp", every="2m").agg(
+            [
+                ((pl.col("severity").filter(pl.col("severity") == t)).count()).alias(
+                    f"{t}_rpm"
+                )
+                for t in LOG_TYPE
+            ]
+        )
 
-    grouped_measurements_df = measurements_df.groupby_dynamic(
-        "timestamp", every="2m"
-    ).agg(
-        [
-            ((pl.col("variant").filter(pl.col("variant") == t)).count()).alias(
-                f"{t}_rpm"
-            )
-            for t in measurement_data_type
-        ]
-    )
-    grouped_logs_df = logs_df.groupby_dynamic("timestamp", every="2m").agg(
-        [
-            ((pl.col("severity").filter(pl.col("severity") == t)).count()).alias(
-                f"{t}_rpm"
-            )
-            for t in log_data_type
-        ]
-    )
+        grouped_measurements_df.write_parquet(MEASUREMENT_DF_CACHE_PATH)
+        grouped_logs_df.write_parquet(LOGS_DF_CACHE_PATH)
 
-    # print(grouped_measurements_df)
-    # print(grouped_logs_df)
+    else:
+        grouped_measurements_df = pl.read_parquet(MEASUREMENT_DF_CACHE_PATH)
+        grouped_logs_df = pl.read_parquet(LOGS_DF_CACHE_PATH)
+
+    print(grouped_measurements_df)
+    print(grouped_logs_df)
 
     plt.subplots(
         1,
@@ -71,9 +114,18 @@ if __name__ == "__main__":
         title="Used code version over time",
         xaxis_scale="days",
     ) as p:
-        for t in measurement_data_type:
+        for t in MEASUREMENT_TYPE:
             xs = grouped_measurements_df.get_column("timestamp")
-            ys = [y + 0 for y in grouped_measurements_df.get_column(f"{t}_rpm")]
-            p.scatter(xs, ys, s=1, color="red", alpha=0.5)
+            ys = [
+                y + DATA_TYPE_OFFSET[t]
+                for y in grouped_measurements_df.get_column(f"{t}_rpm")
+            ]
+            p.scatter(xs, ys, s=1, color=DATA_TYPE_COLOR[t], alpha=0.5)
+        for t in LOG_TYPE:
+            xs = grouped_logs_df.get_column("timestamp")
+            ys = [
+                y + DATA_TYPE_OFFSET[t] for y in grouped_logs_df.get_column(f"{t}_rpm")
+            ]
+            p.scatter(xs, ys, s=1.2, color=DATA_TYPE_COLOR[t], alpha=0.8)
 
     utils.save_plot(f"sensor_activity_{sensor_number}.png")
