7ea9fa827900672eae23936bb98d939ed059ea61
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-17 14:17:09 +0100

    feat(analysis): plot used code version (2)
    refine plot

--- analysis/run.py
+++ analysis/run.py
@@ -1,8 +1,22 @@
+import colorsys
+import math
 from matplotlib import pyplot as plt
 from src import utils
+import matplotlib.patches as mpatches
 
 sensor_offsets = {
-    f"tum-esm-midcost-raspi-{i+1}": round(0.285 - (i * 0.03), 2) for i in range(20)
+    f"tum-esm-midcost-raspi-{i+1}": -round((i * 0.03) + (0.03 * math.floor(i / 5)), 2)
+    for i in range(20)
+}
+sensor_colors = {
+    f"tum-esm-midcost-raspi-{i+1}": "#"
+    + "".join(
+        [
+            hex(int(c * 255))[2:].zfill(2)
+            for c in colorsys.hls_to_rgb((i * 12 / 360), 0.7, 0.8)
+        ]
+    )
+    for i in range(20)
 }
 
 if __name__ == "__main__":
@@ -11,7 +25,7 @@ if __name__ == "__main__":
     activities = utils.SQLQueries.fetch_sensor_code_version_activity(config)
     code_versions = list(set([a.code_version for a in activities]))
     code_version_offsets = {
-        v: i
+        v: i * 0.9
         for i, v in enumerate(
             list(sorted(code_versions, key=lambda v: int(v.split(".")[-1])))
         )
@@ -29,15 +43,25 @@ if __name__ == "__main__":
         subplot_col_count=1,
         subplot_number=1,
         xlabel="UTC time",
-        ylabel="used code version",
-        legend="auto",
-        title=r"User code version over time",
+        ylabel="code version with\nactive measurement data",
+        title="Used code version over time",
         xaxis_scale="months",
     ) as p:
         p.set_yticks(
             list(code_version_offsets.values()),
             list(code_version_offsets.keys()),
         )
+
+        min_timestamp = min([a.first_measurement_timestamp for a in activities])
+        max_timestamp = max([a.last_measurement_timestamp for a in activities])
+
+        for code_version in code_version_offsets.keys():
+            for sensor_name in sensor_offsets.keys():
+                xs = [min_timestamp, max_timestamp]
+                ys = [
+                    code_version_offsets[code_version] + sensor_offsets[sensor_name]
+                ] * 2
+                p.plot(xs, ys, linewidth=5, color="#eeeeff")
 
         for a in activities:
             xs = [
@@ -47,11 +71,9 @@ if __name__ == "__main__":
             ys = [
                 code_version_offsets[a.code_version] + sensor_offsets[a.sensor_name]
             ] * 2
-            p.plot(
-                xs,
-                ys,
-                linewidth=2,
-                label=f"{a.sensor_name} {a.code_version}",
-            )
+            p.plot(xs, ys, linewidth=5, color=sensor_colors[a.sensor_name])
+
+        patches = [mpatches.Patch(color=v, label=k) for k, v in sensor_colors.items()]
+        p.legend(handles=patches, bbox_to_anchor=(1.02, 1))
 
     utils.save_plot("used_code_versions.png")
--- analysis/src/queries/used_sensor_software.sql
+++ analysis/src/queries/used_sensor_software.sql
@@ -5,14 +5,14 @@ SELECT
     MAX(measurements.creation_timestamp) as last_measurement_timestamp
 FROM sensors
 JOIN configurations USING (sensor_identifier)
-JOIN measurements USING (sensor_identifier)
+JOIN measurements USING (sensor_identifier, revision)
 WHERE (
     (NOT ((configurations.configuration ->> 'version') IS NULL)) AND
-    (measurements.revision = configurations.revision) AND
     (sensors.sensor_name LIKE 'tum-esm-midcost-raspi-%') AND
     (NOT ((configurations.configuration ->> 'version') IN (
         '0.1.0-alpha.5', '0.1.0-alpha.6', '0.1.0-alpha.7', '0.1.0-alpha.8', '0.1.0-alpha.9', '0.1.0-alpha.10'
-    )))
+    ))) AND
+    (measurements.creation_timestamp > now() - interval '3 days')
 )
 GROUP BY sensors.sensor_name, sensor_code_version
 ORDER BY sensor_code_version DESC, sensors.sensor_name ASC;
