0ee7e2690ac83143425835852fbc59ff31cd19ec
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-17 12:37:26 +0100

    feat(analysis): remove useless sensor list from config

--- analysis/src/custom_types/config.py
+++ analysis/src/custom_types/config.py
@@ -9,7 +9,7 @@ class DatabaseConfig(BaseModel):
     port: int
     user: str
     password: str
-    database: str
+    db_name: str
 
     # validators
     _val_host = validator("host", pre=True, allow_reuse=True)(
@@ -24,25 +24,7 @@ class DatabaseConfig(BaseModel):
     _val_password = validator("password", pre=True, allow_reuse=True)(
         validate_str(),
     )
-    _val_database = validator("database", pre=True, allow_reuse=True)(
-        validate_str(),
-    )
-
-    class Config:
-        extra = "forbid"
-
-
-class SensorConfig(BaseModel):
-    """content of field `config.sensors` in file `config.json`"""
-
-    sensor_name: str
-    sensor_identifier: str
-
-    # validators
-    _val_sensor_name = validator("sensor_name", pre=True, allow_reuse=True)(
-        validate_str(),
-    )
-    _val_sensor_identifier = validator("sensor_identifier", pre=True, allow_reuse=True)(
+    _val_db_name = validator("db_name", pre=True, allow_reuse=True)(
         validate_str(),
     )
 
@@ -54,7 +36,6 @@ class Config(BaseModel):
     """content of file `config.json`"""
 
     database: DatabaseConfig
-    sensors: list[SensorConfig]
 
     class Config:
         extra = "forbid"
