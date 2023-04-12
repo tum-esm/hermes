d8e01c83025e8112885e013425f59d852be5fe37
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-12 16:16:59 +0200

    feat(dashboard): implement ui (3)
    refine landing page

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -2,6 +2,23 @@ import { SENSOR_IDS } from "@/components/constants";
 import { ICONS } from "@/components/icons";
 import Link from "next/link";
 
+function SensorListItem(props: { sensorName: string }) {
+  return (
+    <li
+      key={props.sensorName}
+      className="flex w-full flex-row items-center justify-start gap-x-3 px-5 py-4"
+    >
+      <div className="h-1.5 w-1.5 rounded-full bg-green-500" />
+      <p className="text-slate-800 group-hover:text-black">
+        {props.sensorName}
+      </p>
+      <div className="flex-grow"></div>
+      <div className="w-4 text-slate-800 group-hover:text-black">
+        {ICONS.chevronRight}
+      </div>
+    </li>
+  );
+}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-y-6 bg-slate-50 px-8 py-16">
@@ -11,17 +28,9 @@ export default function Home() {
           {Object.keys(SENSOR_IDS).map((sensorName) => (
             <Link
               href={`/sensor/${sensorName}`}
-              className="block border-b border-slate-100 last:border-none hover:bg-slate-100"
+              className="group block border-b border-slate-100 last:border-none hover:bg-slate-50"
             >
-              <li
-                key={sensorName}
-                className="flex w-full flex-row items-center justify-start gap-x-3 p-4"
-              >
-                <div className="w-5 text-green-700">{ICONS.router}</div>
-                <p className="text-slate-900">{sensorName}</p>
-                <div className="flex-grow"></div>
-                <div className="w-5 text-slate-700">{ICONS.chevronRight}</div>
-              </li>
+              <SensorListItem sensorName={sensorName} />
             </Link>
           ))}
         </ul>
