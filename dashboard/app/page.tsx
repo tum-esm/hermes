d133cb9c618dce79217c639f7b28cee99b506a8c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-12 21:09:17 +0200

    feat(dashboard): implement ui (5)
    add legend for status dots
    add details to sensor rows

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -3,22 +3,60 @@ import { ICONS } from "@/components/icons";
 import Link from "next/link";
 
 // TODO: render server status and URL
+// TODO: render point color legend
+
+function PointColorLegend() {
+  return (
+    <div className="flex flex-row items-center justify-start w-full max-w-4xl px-5 py-3 text-sm border-t gap-x-4 border-slate-200 bg-slate-50">
+      <h2 className="font-medium whitespace-nowrap">
+        Station Status Indicators
+      </h2>
+      <div className="flex flex-col items-start justify-start w-full gap-y-1">
+        <div className="flex w-full flex-row items-center justify-start gap-x-1.5">
+          <div className="w-2 h-2 bg-green-500 rounded-sm" />
+          <p className="text-slate-800">
+            = only measurement data in the last 30 minutes
+          </p>
+        </div>
+        <div className="flex w-full flex-row items-center justify-start gap-x-1.5">
+          <div className="w-2 h-2 bg-yellow-500 rounded-sm" />
+          <p className="text-slate-800">
+            = measurement data and logs in the last 30 minutes
+          </p>
+        </div>
+        <div className="flex w-full flex-row items-center justify-start gap-x-1.5">
+          <div className="w-2 h-2 bg-red-500 rounded-sm" />
+          <p className="text-slate-800">= only logs in the last 30 minutes</p>
+        </div>
+        {/* the same but a slate-500 color for no data in the last 30 minutes */}
+        <div className="flex w-full flex-row items-center justify-start gap-x-1.5">
+          <div className="w-2 h-2 rounded-sm bg-slate-500" />
+          <p className="text-slate-800">= no data in the last 30 minutes</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function SensorListItem(props: { sensorName: string }) {
-  // TODO: fetch measurement and logs data
-  // TODO: green if measurements in the last 10 minutes
-  // TODO: yellow if no measurements in the last 10 minutes but logs in the last 30 minutes (doing reset routine)
-  // TODO: red otherwise
+  // TODO: fetch data and logs
+  // TODO: determine color
 
   return (
     <li
       key={props.sensorName}
-      className="flex w-full flex-row items-center justify-start gap-x-3 px-5 py-4"
+      className="flex flex-row items-center justify-start w-full px-5 py-4 gap-x-4"
     >
-      <div className="h-1.5 w-1.5 rounded-full bg-green-500" />
-      <p className="text-slate-800 group-hover:text-black">
-        {props.sensorName}
-      </p>
+      <div className="w-2 h-2 bg-green-500 rounded-sm" />
+      <div className="flex flex-col leading-tight text-slate-800 group-hover:text-black">
+        <p className="mr-2 font-medium">{props.sensorName}</p>
+        <p className="text-xs">
+          <span className="inline-block w-14">last data:</span> ...
+        </p>
+        <p className="text-xs">
+          <span className="inline-block w-14">last log:</span> ...
+        </p>
+      </div>
       <div className="flex-grow"></div>
       <div className="w-4 text-slate-800 group-hover:text-black">
         {ICONS.chevronRight}
@@ -29,19 +67,20 @@ function SensorListItem(props: { sensorName: string }) {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-y-6 bg-slate-50 px-8 py-16">
+    <main className="flex flex-col items-center min-h-screen px-8 py-16 gap-y-6 bg-slate-50">
       <h1 className="text-3xl font-medium">TUM ESM - Acropolis</h1>
-      <div className="w-full max-w-4xl overflow-hidden rounded-lg border border-slate-300 bg-white shadow">
+      <div className="w-full max-w-3xl overflow-hidden bg-white border rounded-lg shadow border-slate-300">
         <ul>
           {Object.keys(SENSOR_IDS).map((sensorName) => (
             <Link
               href={`/sensor/${sensorName}`}
-              className="group block border-b border-slate-100 last:border-none hover:bg-slate-50"
+              className="block border-b group border-slate-100 last:border-none hover:bg-slate-50"
             >
               <SensorListItem sensorName={sensorName} />
             </Link>
           ))}
         </ul>
+        <PointColorLegend />
       </div>
     </main>
   );
--- dashboard/components/icons.tsx
+++ dashboard/components/icons.tsx
@@ -6,11 +6,11 @@ export const ICONS = {
       xmlns="http://www.w3.org/2000/svg"
       className="icon icon-tabler icon-tabler-router"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="3"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M3 13m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
@@ -28,11 +28,11 @@ export const ICONS = {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="3"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M17 13h2a2 2 0 0 1 2 2v2m-.588 3.417c-.362 .36 -.861 .583 -1.412 .583h-14a2 2 0 0 1 -2 -2v-4a2 2 0 0 1 2 -2h8"></path>
@@ -48,11 +48,11 @@ export const ICONS = {
       xmlns="http://www.w3.org/2000/svg"
       className="icon icon-tabler icon-tabler-chevron-right"
       viewBox="0 0 24 24"
-      stroke-width="3"
+      strokeWidth="3"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M9 6l6 6l-6 6"></path>
