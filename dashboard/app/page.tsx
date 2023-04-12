c3c35eda81564690444167a91f10932e13a1aa74
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-12 16:03:55 +0200

    feat(dashboard): implement ui (2)
    implement landing page

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,18 +1,31 @@
 import { SENSOR_IDS } from "@/components/constants";
+import { ICONS } from "@/components/icons";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello world!</h1>
-      <p>Sensors:</p>
-      <ul>
-        {Object.keys(SENSOR_IDS).map((sensorName) => (
-          <li key={sensorName}>
-            <Link href={`/sensor/${sensorName}`}>{sensorName}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <main className="flex min-h-screen flex-col items-center gap-y-6 bg-slate-50 px-8 py-16">
+      <h1 className="text-3xl font-medium">TUM ESM - Acropolis</h1>
+      <div className="w-full max-w-4xl overflow-hidden rounded-lg border border-slate-300 bg-white shadow">
+        <ul>
+          {Object.keys(SENSOR_IDS).map((sensorName) => (
+            <Link
+              href={`/sensor/${sensorName}`}
+              className="block border-b border-slate-100 last:border-none hover:bg-slate-100"
+            >
+              <li
+                key={sensorName}
+                className="flex w-full flex-row items-center justify-start gap-x-3 p-4"
+              >
+                <div className="w-5 text-green-700">{ICONS.router}</div>
+                <p className="text-slate-900">{sensorName}</p>
+                <div className="flex-grow"></div>
+                <div className="w-5 text-slate-700">{ICONS.chevronRight}</div>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
---
+++ dashboard/components/icons.tsx
@@ -0,0 +1,61 @@
+// source: https://tabler-icons.io/
+
+export const ICONS = {
+  router: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-router"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M3 13m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+      <path d="M17 17l0 .01"></path>
+      <path d="M13 17l0 .01"></path>
+      <path d="M15 13l0 -2"></path>
+      <path d="M11.75 8.75a4 4 0 0 1 6.5 0"></path>
+      <path d="M8.5 6.5a8 8 0 0 1 13 0"></path>
+    </svg>
+  ),
+  routerOff: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-router-off"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M17 13h2a2 2 0 0 1 2 2v2m-.588 3.417c-.362 .36 -.861 .583 -1.412 .583h-14a2 2 0 0 1 -2 -2v-4a2 2 0 0 1 2 -2h8"></path>
+      <path d="M17 17v.01"></path>
+      <path d="M13 17v.01"></path>
+      <path d="M12.226 8.2a4 4 0 0 1 6.024 .55"></path>
+      <path d="M9.445 5.407a8 8 0 0 1 12.055 1.093"></path>
+      <path d="M3 3l18 18"></path>
+    </svg>
+  ),
+  chevronRight: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-chevron-right"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M9 6l6 6l-6 6"></path>
+    </svg>
+  ),
+};
