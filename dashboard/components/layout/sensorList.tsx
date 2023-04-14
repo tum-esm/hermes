f5e3dbdc152a6b5ff7169a5f7768d0f89b394e44
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-14 18:09:23 +0200

    feat(dashboard): connect datastreams (8)
    render list items correctly

--- dashboard/components/layout/sensorList.tsx
+++ dashboard/components/layout/sensorList.tsx
@@ -1,10 +1,51 @@
 import { SENSOR_IDS } from "@/components/constants";
+import { determinSensorStatus, useNetworkStore } from "@/components/state";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { maxBy } from "lodash";
+
+const variantToBgColor = {
+  online: "bg-green-500",
+  unstable: "bg-yellow-500",
+  error: "bg-red-500",
+  offline: "bg-slate-500",
+};
+
+function renderTimeString(time: number | undefined): string {
+  if (time === undefined) {
+    return "-";
+  }
+  const diff = new Date().getTime() / 1000 - time;
+  const formatter = new Intl.RelativeTimeFormat("en");
+  if (diff < 60) {
+    return formatter.format(Math.floor(-diff), "seconds");
+  } else if (diff < 60 * 60) {
+    return formatter.format(Math.floor(-diff / 60), "minutes");
+  } else if (diff < 60 * 60 * 24 * 2) {
+    return formatter.format(Math.floor(-diff / (60 * 60)), "hours");
+  } else {
+    return formatter.format(Math.floor(-diff / (60 * 60 * 24)), "days");
+  }
+}
 
 function SensorListItem(props: { sensorName: string }) {
   // TODO: connect data and logs
   // TODO: determine color
+
+  const networkState = useNetworkStore((state) => state.state);
+  const sensorState = networkState.filter(
+    (sensor) => sensor.sensorId === SENSOR_IDS[props.sensorName]
+  )[0];
+
+  const sensorStatus = determinSensorStatus(sensorState);
+  const lastDataTime = maxBy(
+    sensorState?.data,
+    (data) => data.creation_timestamp
+  )?.creation_timestamp;
+  const lastLogTime = maxBy(
+    sensorState?.logs,
+    (log) => log.max_creation_timestamp
+  )?.max_creation_timestamp;
 
   const pathname = usePathname();
   const isSelected = pathname === `/sensor/${props.sensorName}`;
@@ -19,7 +60,14 @@ function SensorListItem(props: { sensorName: string }) {
           : "hover:bg-slate-50")
       }
     >
-      <div className="h-2 w-2 rounded-sm bg-green-500" />
+      <div
+        className={
+          "h-2 w-2 rounded-sm " +
+          (sensorStatus === undefined
+            ? "bg-slate-200"
+            : variantToBgColor[sensorStatus])
+        }
+      />
       <div
         className={
           "flex flex-col leading-tight " +
@@ -30,10 +78,12 @@ function SensorListItem(props: { sensorName: string }) {
       >
         <p className="mr-2 font-medium">{props.sensorName}</p>
         <p className="text-xs">
-          <span className="inline-block w-14">last data:</span> ...
+          <span className="inline-block w-14">last data:</span>{" "}
+          {renderTimeString(lastDataTime)}
         </p>
         <p className="text-xs">
-          <span className="inline-block w-14">last log:</span> ...
+          <span className="inline-block w-14">last log:</span>{" "}
+          {renderTimeString(lastDataTime)}
         </p>
       </div>
     </li>
