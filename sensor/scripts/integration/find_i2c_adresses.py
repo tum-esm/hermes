cd73f9dee00b89c5795721f6a56a68571f96fb10
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-22 10:45:53 +0200

    refactor(sensor): use sht45 and bme289 instead of sht21 (2)
    add script to find i2c adresses

---
+++ sensor/scripts/integration/find_i2c_adresses.py
@@ -0,0 +1,18 @@
+import os
+from smbus2 import SMBus
+
+for b in os.listdir("/dev"):
+    if os.path.isdir(os.path.join("/dev", b)):
+        continue
+
+    try:
+        bus = SMBus(f"/dev/{b}")
+
+        for a in range(0x000, 0xFFF):
+            try:
+                print(f"{b} -> {hex(a)} -> {bus.read_byte_data(a, 0)}")
+            except OSError:
+                pass
+        bus.close()
+    except (PermissionError, OSError):
+        pass
