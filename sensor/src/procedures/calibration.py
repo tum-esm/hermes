63e23419fca5ea652a0310f009703727a2d14ebb
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-26 14:51:43 +0200

    refactor(sensor): refactor calibration (2)
    simplify calibration procedure, add testing mode, where logs are only printed

--- sensor/src/procedures/calibration.py
+++ sensor/src/procedures/calibration.py
@@ -1,7 +1,29 @@
 from datetime import datetime
+import json
 import math
 import time
+from typing import Generator
 from src import custom_types, utils, hardware
+
+
+def _ensure_section_duration(duration: float) -> Generator[None, None, None]:
+    """Make sure that the duration of the section is at least
+    the given duration.
+
+    Usage:
+
+    ```python
+    # do one measurement every 6 seconds
+    with ensure_section_duration(6):
+        do_measurement()
+    ```
+    """
+
+    start_time = time.time()
+    yield
+    remaining_loop_time = start_time + duration - time.time()
+    if remaining_loop_time > 0:
+        time.sleep(remaining_loop_time)
 
 
 class CalibrationProcedure:
@@ -11,89 +33,83 @@ class CalibrationProcedure:
         self,
         config: custom_types.Config,
         hardware_interface: hardware.HardwareInterface,
+        testing: bool = False,
     ) -> None:
-        self.logger, self.config = utils.Logger(origin="calibration-procedure"), config
+        self.logger = utils.Logger(
+            origin="calibration-procedure", print_to_console=testing
+        )
+        self.config = config
         self.hardware_interface = hardware_interface
         self.active_mqtt_queue = utils.ActiveMQTTQueue()
 
     def run(self) -> None:
-        calibration_time = datetime.utcnow().timestamp()
-
-        # randomize calibration gas order
-        result = custom_types.CalibrationProcedureData(
-            gases=self.config.calibration.gases, readings=[]
+        self.logger.info(
+            f"running calibration procedure at timestamp {calibration_time}"
         )
-
-        # save the currently active valve input for later
+        calibration_time = datetime.utcnow().timestamp()
+        result = custom_types.CalibrationProcedureData(
+            gases=self.config.calibration.gases, readings=[], timestamps=[]
+        )
         previous_measurement_valve_input = self.hardware_interface.valves.active_input
 
-        # start the calibration sampling
+        self.logger.debug("setting the CO2 sensor to calibration mode")
         self.hardware_interface.co2_sensor.start_calibration_sampling()
-        self.hardware_interface.co2_sensor.set_filter_setting(
-            average=self.config.calibration.sampling.seconds_per_sample
+        self.hardware_interface.co2_sensor.set_filter_setting(average=5)
+        self.hardware_interface.pump.set_desired_pump_speed(
+            unit="litres_per_minute", value=0.5
         )
 
         for gas in self.config.calibration.gases:
             self.hardware_interface.valves.set_active_input(gas.valve_number)
 
-            # flush tube with calibration gas
-            self.hardware_interface.pump.set_desired_pump_speed(
-                unit="litres_per_minute",
-                value=self.config.calibration.flushing.pumped_litres_per_minute,
-            )
-            time.sleep(self.config.calibration.flushing.seconds)
-
-            # sample measurements and average them
-            self.hardware_interface.pump.set_desired_pump_speed(
-                unit="litres_per_minute",
-                value=self.config.calibration.sampling.pumped_litres_per_minute,
-            )
             gas_readings: list[custom_types.CO2SensorData] = []
-            for _ in range(self.config.calibration.sampling.sample_count):
-                start_time = time.time()
-                gas_readings.append(
-                    self.hardware_interface.co2_sensor.get_current_concentration()
-                )
-                elapsed_time = time.time() - start_time
-                remaining_loop_time = (
-                    self.config.calibration.sampling.seconds_per_sample - elapsed_time
-                )
-                if remaining_loop_time > 0:
-                    time.sleep(remaining_loop_time)
+            timestamps: list[float] = []
+            number_of_readings = math.ceil(
+                self.config.calibration.seconds_per_gas_bottle / 6
+            )
+            for _ in range(number_of_readings):
+                with _ensure_section_duration(6):
+                    timestamps.append(datetime.utcnow().timestamp())
+                    gas_readings.append(
+                        self.hardware_interface.co2_sensor.get_current_concentration()
+                    )
 
             result.readings.append(gas_readings)
+            result.timestamps.append(gas_readings)
 
         # start the calibration sampling, reset CO2 sensor to default filters
         self.hardware_interface.co2_sensor.stop_calibration_sampling()
         self.hardware_interface.co2_sensor.set_filter_setting()
 
-        # send calibration result via mqtt
-        self.active_mqtt_queue.enqueue_message(
-            self.config,
-            custom_types.MQTTDataMessageBody(
-                revision=self.config.revision,
-                timestamp=round(time.time(), 2),
-                value=custom_types.MQTTCalibrationData(
-                    variant="calibration", data=result
-                ),
-            ),
-        )
-
         # clean up tube again
         self.hardware_interface.valves.set_active_input(
             previous_measurement_valve_input
         )
-        self.hardware_interface.pump.set_desired_pump_speed(
-            unit="litres_per_minute",
-            value=self.config.calibration.cleaning.pumped_litres_per_minute,
-        )
-        time.sleep(self.config.calibration.cleaning.seconds)
+
+        # send calibration result via mqtt
+        if self.testing:
+            self.logger.info(
+                f"calibration result is available",
+                details=json.dumps(result.dict()),
+            )
+        else:
+            self.active_mqtt_queue.enqueue_message(
+                self.config,
+                custom_types.MQTTDataMessageBody(
+                    revision=self.config.revision,
+                    timestamp=round(time.time(), 2),
+                    value=custom_types.MQTTCalibrationData(
+                        variant="calibration", data=result
+                    ),
+                ),
+            )
 
         # save last calibration time
         state = utils.StateInterface.read()
         state.last_calibration_time = calibration_time
         utils.StateInterface.write(state)
 
+    # TODO: log more
     def is_due(self) -> bool:
         """returns true when calibration procedure should run now"""
 
