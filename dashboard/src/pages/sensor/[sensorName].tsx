d63fc9fa8cfe44a7909f2c1f9f292b43e7d71959
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-02 16:18:39 +0200

    feat(dashboard): add raw logs #76 (2)
    refactor sensor page code

--- dashboard/src/pages/sensor/[sensorName].tsx
+++ dashboard/src/pages/sensor/[sensorName].tsx
@@ -7,6 +7,37 @@ import { determinSensorStatus, renderTimeString } from "@/src/utils/functions";
 import { maxBy } from "lodash";
 import { VARIANT_TO_PILL_COLOR } from "@/src/utils/colors";
 
+function Tab(props: { visible: boolean; children: React.ReactNode }) {
+  return (
+    <div
+      className={
+        (props.visible ? "block" : "hidden") + " flex w-full flex-col gap-y-4"
+      }
+    >
+      {props.children}
+    </div>
+  );
+}
+
+function Card(props: {
+  key: string | number;
+  title: React.ReactNode;
+  subtitle: React.ReactNode;
+  bottom: any;
+}) {
+  return (
+    <div className="flex flex-col w-full overflow-hidden bg-white border rounded-lg shadow border-slate-300">
+      <div className="flex flex-row items-center justify-start px-3 pt-2 pb-1 text-sm gap-x-2 text-slate-900">
+        {props.title}
+      </div>
+      <div className="pb-2 text-xs pl-7">{props.subtitle}</div>
+      <div className="px-3 py-2 text-xs leading-tight border-t whitespace-break-spaces border-slate-200 bg-slate-50 text-slate-900 text-opacity-80">
+        {JSON.stringify(props.bottom, null, 4)}
+      </div>
+    </div>
+  );
+}
+
 export function getStaticPaths() {
   return {
     paths: Object.keys(SENSOR_IDS).map((sensorName) => ({
@@ -24,14 +55,15 @@ export function getStaticProps(context: any) {
   };
 }
 
-const TAB_NAMES: ("data" | "logs (aggregated)")[] = [
+const TAB_NAMES: ("data" | "logs" | "logs (aggregated)")[] = [
   "data",
+  "logs",
   "logs (aggregated)",
 ];
 
 export default function Page({ sensorName }: { sensorName: string }) {
   const sensorId = SENSOR_IDS[sensorName];
-  const [tab, setTab] = useState<"data" | "logs (aggregated)">("data");
+  const [tab, setTab] = useState<"data" | "logs" | "logs (aggregated)">("data");
 
   const networkState = useNetworkStore((state) => state.state);
   const sensorState = networkState.filter(
@@ -109,83 +141,72 @@ export default function Page({ sensorName }: { sensorName: string }) {
       {sensorStatus === undefined && <p className="px-4">loading...</p>}
       {sensorStatus !== undefined && (
         <>
-          <div
-            className={
-              (tab === "data" ? "block" : "hidden") +
-              " flex w-full flex-col gap-y-4"
-            }
-          >
+          <Tab visible={tab == "data"}>
             {sensorState.data
               ?.sort((a, b) => b.creation_timestamp - a.creation_timestamp)
               .map((data) => (
-                <div
-                  className="flex flex-col w-full overflow-hidden bg-white border rounded-lg shadow border-slate-300"
-                  key={data.creation_timestamp}
-                >
-                  <div className="flex flex-row items-center justify-start px-3 pt-2 pb-1 text-sm gap-x-2 text-slate-900">
-                    <div className="flex-shrink-0 w-2 h-2 bg-blue-500 rounded-sm" />
-                    <div>
-                      {new Date(
-                        data.creation_timestamp * 1000
-                      ).toLocaleString()}{" "}
-                      (local time)
-                    </div>
-                  </div>
-                  <div className="pb-2 text-xs pl-7">
-                    {renderTimeString(data.creation_timestamp)}
-                  </div>
-                  <div className="px-3 py-2 text-xs leading-tight border-t whitespace-break-spaces border-slate-200 bg-slate-50 text-slate-900 text-opacity-80">
-                    {JSON.stringify(data, null, 4)}
-                  </div>
-                </div>
+                <>
+                  <Card
+                    key={data.creation_timestamp}
+                    title={
+                      <>
+                        <div className="flex-shrink-0 w-2 h-2 bg-blue-500 rounded-sm" />
+                        <div>
+                          {new Date(
+                            data.creation_timestamp * 1000
+                          ).toLocaleString()}{" "}
+                          (local time)
+                        </div>
+                      </>
+                    }
+                    subtitle={renderTimeString(data.creation_timestamp)}
+                    bottom={data}
+                  />
+                </>
               ))}
-          </div>
-          <div
-            className={
-              (tab === "logs (aggregated)" ? "block" : "hidden") +
-              " flex w-full flex-col gap-y-4"
-            }
-          >
+          </Tab>
+
+          <Tab visible={tab == "logs (aggregated)"}>
             {sensorState.aggregatedLogs
               ?.sort(
                 (a, b) => b.max_creation_timestamp - a.max_creation_timestamp
               )
               .map((log) => (
-                <div
-                  className="flex flex-col w-full overflow-hidden bg-white border rounded-lg shadow border-slate-300"
+                <Card
                   key={log.subject}
-                >
-                  <div className="flex flex-row items-center justify-start px-3 pt-2 pb-1 text-sm gap-x-2 text-slate-900">
-                    <div
-                      className={
-                        "h-2 w-2 flex-shrink-0 rounded-sm " +
-                        (log.severity === "info"
-                          ? "bg-slate-300"
-                          : log.severity === "warning"
-                          ? "bg-yellow-500"
-                          : "bg-red-500")
-                      }
-                    />
-                    <div>
-                      {log.subject.length > 100
-                        ? `${log.subject.slice(0, 100)} ...`
-                        : log.subject}
-                    </div>
-                  </div>
-                  <div className="pb-2 text-xs pl-7">
-                    last occured {renderTimeString(log.max_creation_timestamp)}{" "}
-                    -{" "}
-                    {new Date(
-                      log.max_creation_timestamp * 1000
-                    ).toLocaleString()}{" "}
-                    (local time)
-                  </div>
-                  <div className="px-3 py-2 text-xs leading-tight border-t whitespace-break-spaces border-slate-200 bg-slate-50 text-slate-900 text-opacity-80">
-                    {JSON.stringify(log, null, 4)}
-                  </div>
-                </div>
+                  title={
+                    <>
+                      <div
+                        className={
+                          "h-2 w-2 flex-shrink-0 rounded-sm " +
+                          (log.severity === "info"
+                            ? "bg-slate-300"
+                            : log.severity === "warning"
+                            ? "bg-yellow-500"
+                            : "bg-red-500")
+                        }
+                      />
+                      <div>
+                        {log.subject.length > 100
+                          ? `${log.subject.slice(0, 100)} ...`
+                          : log.subject}
+                      </div>
+                    </>
+                  }
+                  subtitle={
+                    <>
+                      last occured{" "}
+                      {renderTimeString(log.max_creation_timestamp)} -{" "}
+                      {new Date(
+                        log.max_creation_timestamp * 1000
+                      ).toLocaleString()}{" "}
+                      (local time)
+                    </>
+                  }
+                  bottom={log}
+                />
               ))}
-          </div>
+          </Tab>
         </>
       )}
     </>
