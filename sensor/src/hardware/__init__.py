b0d635a24c54868d28c82ffc63b5b73a788ce8f3
patrickaigner <pataigner.login@gmail.com>
2023-07-10 09:55:16 +0200

    split CO2 sensor from teardown

--- sensor/src/hardware/__init__.py
+++ sensor/src/hardware/__init__.py
@@ -15,9 +15,7 @@ from .valves import ValveInterface
 from .wind_sensor import WindSensorInterface
 
 # global lock over all software versions
-hardware_lock = filelock.FileLock(
-    "/home/pi/Documents/hermes/hermes-hardware.lock", timeout=5
-)
+hardware_lock = filelock.FileLock("/home/pi/Documents/hermes/hermes-hardware.lock", timeout=5)
 
 
 class HardwareInterface:
@@ -59,9 +57,7 @@ class HardwareInterface:
 
         # heated enclosure communication with repairing
         # routine is running in a separate thread
-        if (
-            not self.testing
-        ) and self.config.active_components.communicate_with_heated_enclosure:
+        if (not self.testing) and self.config.active_components.communicate_with_heated_enclosure:
             HeatedEnclosureThread.init(config)
         else:
             self.logger.debug("skipping heated enclosure communication")
@@ -77,7 +73,7 @@ class HardwareInterface:
         HeatedEnclosureThread.check_errors()
 
     def teardown(self) -> None:
-        """ends all hardware/system connections"""
+        """ends all hardware/system connections excluding the CO2 sensor"""
         self.logger.info("running hardware teardown")
 
         if not hardware_lock.is_locked:
@@ -86,7 +82,6 @@ class HardwareInterface:
 
         # measurement sensors
         self.air_inlet_bme280_sensor.teardown()
-        self.co2_sensor.teardown()
         self.wind_sensor.teardown()
 
         # measurement actors
@@ -100,6 +95,16 @@ class HardwareInterface:
 
         # release lock
         hardware_lock.release()
+
+    def co2_sensor_teardown(self) -> None:
+        """performs a teardown for the CO2 sensor"""
+        self.logger.info("performing CO2 sensor teardown")
+
+        if not hardware_lock.is_locked:
+            self.logger.info("not tearing down due to disconnected hardware")
+            return
+
+        self.co2_sensor.teardown()
 
     def reinitialize(self, config: custom_types.Config) -> None:
         """reinitialize after an unsuccessful update"""
@@ -125,9 +130,7 @@ class HardwareInterface:
         )
         self.ups = UPSInterface(config, testing=self.testing)
 
-        if (
-            not self.testing
-        ) and self.config.active_components.communicate_with_heated_enclosure:
+        if (not self.testing) and self.config.active_components.communicate_with_heated_enclosure:
             HeatedEnclosureThread.init(config)
         else:
             self.logger.debug("skipping heated enclosure communication")
