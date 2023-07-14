9386e839b600d7ee8e32e35797206e8f63c2c989
Felix Böhm <felix@felixboehm.dev>
2023-07-15 00:08:53 +0200

    refactor(server): Replace regex look-arounds with basic regex logic to circumvent pydantic v2's limitation

--- server/app/validation/mqtt.py
+++ server/app/validation/mqtt.py
@@ -6,47 +6,47 @@ import app.validation.constants as constants
 import app.validation.types as types
 
 
-class Acknowledgement(types.Model):
+class Acknowledgement(types.StrictModel):
     timestamp: types.Timestamp
     revision: types.Revision
     success: bool  # Records if the configuration was processed successfully
 
 
-class AcknowledgementsMessage(types.Model):
+class AcknowledgementsMessage(types.StrictModel):
     values: pydantic.conlist(item_type=Acknowledgement, min_length=1) = pydantic.Field(
         ..., alias="heartbeats"
     )
 
 
-class Log(types.Model):
+class Log(types.StrictModel):
     timestamp: types.Timestamp
     revision: types.Revision | None = None
     severity: typing.Literal["info", "warning", "error"]
     subject: str
     details: str | None = None
 
-    @pydantic.validator("subject")
+    @pydantic.field_validator("subject")
     def trim_subject(cls, v):
         return v[: constants.Limit.MEDIUM]
 
-    @pydantic.validator("details")
+    @pydantic.field_validator("details")
     def trim_details(cls, v):
         return v[: constants.Limit.LARGE]
 
 
-class LogsMessage(types.Model):
+class LogsMessage(types.StrictModel):
     values: pydantic.conlist(item_type=Log, min_length=1) = pydantic.Field(
         ..., alias="log_messages"
     )
 
 
-class Measurement(types.Model):
+class Measurement(types.StrictModel):
     timestamp: types.Timestamp
     revision: types.Revision | None = None
     value: types.Measurement
 
 
-class MeasurementsMessage(types.Model):
+class MeasurementsMessage(types.StrictModel):
     values: pydantic.conlist(item_type=Measurement, min_length=1) = pydantic.Field(
         ..., alias="measurements"
     )
--- server/app/validation/types.py
+++ server/app/validation/types.py
@@ -8,11 +8,15 @@ import app.validation.constants as constants
 ########################################################################################
 
 
-class Model(pydantic.BaseModel):
+class StrictModel(pydantic.BaseModel):
     model_config = pydantic.ConfigDict(strict=True, frozen=True, extra="forbid")
 
 
-class Configuration(Model, extra="allow"):
+class LooseModel(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(strict=False, frozen=True, extra="forbid")
+
+
+class Configuration(StrictModel, extra="allow"):
     # TODO Validate the values more thoroughly for min and max limits/lengths
     # number of JSON fields or nesting depth could be interesting as well
     # Or, check the actual size of the JSON / length of the JSON string
@@ -23,16 +27,16 @@ class Configuration(Model, extra="allow"):
 # Types
 ########################################################################################
 
-Name = pydantic.constr(pattern=constants.Pattern.NAME.value)
+Name = pydantic.constr(max_length=64, pattern=constants.Pattern.NAME.value)
 Identifier = pydantic.constr(pattern=constants.Pattern.IDENTIFIER.value)
 Password = pydantic.constr(min_length=8, max_length=constants.Limit.MEDIUM)
+Key = pydantic.constr(max_length=64, pattern=constants.Pattern.KEY.value)
 
 # TODO what are the real min/max values here? How do we handle overflow?
 # During validation somehow, or by handling the database error?
 Revision = pydantic.conint(ge=0, lt=constants.Limit.MAXINT4)
 Timestamp = pydantic.confloat(ge=0, lt=constants.Limit.MAXINT4)
 
-Key = pydantic.constr(pattern=constants.Pattern.KEY.value)
 # TODO Validate the values more thoroughly for min and max limits/lengths
 # number of JSON fields or nesting depth could be interesting as well
 # Or, check the actual size of the JSON / length of the JSON string
