d64664a81e8aa1192d52460862d716f70514e98c
Patrick <patrick.aigner@tum.de>
2023-11-21 15:51:33 +0100

    Refactor Wind Sensor Interface (3)

--- sensor/run_wind_sensor.py
+++ sensor/run_wind_sensor.py
@@ -1,19 +1,16 @@
-import os
-import signal
 import time
-from typing import Any, Optional
-from src import custom_types, utils, hardware, procedures
+from src import utils, hardware
 
 
 try:
     config = utils.ConfigInterface.read()
 except Exception as e:
     raise e
-    
-    
+
+
 wind_sensor = hardware.WindSensorInterface(config, testing=True)
 
-while(True):
+while True:
     print(wind_sensor.get_current_device_status())
     time.sleep(1)
     print(wind_sensor.get_current_wind_measurement())
