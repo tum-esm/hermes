d971766f2468f877b5585cfb0a233d6e452d05db
Patrick <patrick.aigner@tum.de>
2023-11-22 20:20:41 +0100

    Refactor CO2 Sensor Interface (1)

--- sensor/src/utils/serial_interfaces.py
+++ sensor/src/utils/serial_interfaces.py
@@ -15,22 +15,30 @@ class SerialCO2SensorInterface:
             stopbits=1,
         )
 
-    def send_command(self, message: str) -> None:
-        """send a command to the sensor. Puts a "\x1B" string before the
-        command, which will make the sensor wait until previous commands
-        have been processed"""
-        self.serial_interface.write((f"{message}\r\n").encode("utf-8"))
-        self.serial_interface.flush()
+    def send_command(
+        self, message: str, expected_regex: str = r"[^>]*", timeout: float = 8
+    ) -> tuple[Literal["success", "uncomplete", "timeout"], str]:
+        """
+        send a command to the sensor. Puts a "\x1B" string after the
+        command, which will make the sensor wait until commands
+        have been processed.
+        Returns the sensor answer as tuple (indicator, answer).
+        """
+        self.flush_receiver_stream()  # empty input queue
+        self.serial_interface.write((f"{message}\r\n").encode("utf-8"))  # send command
+        self.serial_interface.flush()  # wait until data is written
+        return self.wait_for_answer(
+            expected_regex=expected_regex, timeout=timeout
+        )  # return answer
 
     def flush_receiver_stream(self) -> None:
         """wait 0.2 seconds and then empty the current input queue"""
         time.sleep(0.2)  # wait for outstanding answers from previous commands
-        self.serial_interface.read_all()
+        self.serial_interface.read_all()  # reads everything in queue and throws it away
 
-    def wait_for_answer(self, expected_regex: str = "[^>]*", timeout: float = 8) -> str:
-        expected_pattern = re.compile(
-            r"^(\r\n|\s)*" + expected_regex + r"(\r\n|\s)*>(\r\n|\s)*$"
-        )
+    def wait_for_answer(
+        self, expected_regex: str, timeout: float
+    ) -> tuple[Literal["success", "uncomplete", "timeout"], str]:
         start_time = time.time()
         answer = ""
 
@@ -38,14 +46,19 @@ class SerialCO2SensorInterface:
             received_bytes = self.serial_interface.read_all()
             if received_bytes is not None:
                 answer += received_bytes.decode(encoding="cp1252")
-                if expected_pattern.match(answer) is not None:
-                    return answer
+
+            # return successful answer as it matches expected regex
+            if re.search(expected_regex, answer):
+                return ("success", answer)
+
+            # return answer that is requesting to set the new value in another message
+            # this can happen if only "p" is received instead of "p 1000"
+            # Example answer: "PRESSURE (hPa) : 1000.000 ?"
+            if re.search(r".*\?.*", answer):
+                return ("uncomplete", answer)
 
             if (time.time() - start_time) > timeout:
-                raise TimeoutError(
-                    "CO2 sensor did not answer as expected: expected_regex "
-                    + f"= {repr(expected_regex)}, answer = {repr(answer)}"
-                )
+                return ("timeout", answer)
             else:
                 time.sleep(0.05)
 
