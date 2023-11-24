ecb68532f6507c0e575c541e8b02a18352657d0d
Patrick <patrick.aigner@tum.de>
2023-11-24 16:11:39 +0100

    Refactor UPS Interfaxe (3)

--- sensor/src/hardware/ups.py
+++ sensor/src/hardware/ups.py
@@ -1,5 +1,6 @@
-from src import utils, custom_types
 import gpiozero
+from typing import Optional
+from src import utils, custom_types
 
 UPS_READY_PIN_IN = 5
 UPS_BATTERY_MODE_PIN_IN = 10
@@ -18,93 +19,90 @@ class UPSInterface:
             write_to_file=(not testing),
         )
         self.config = config
-        
+
         self.logger.info("Starting initialization")
-        
-        self.powered_by_grid = None
-        self.battery_is_fully_charged = None
-        self.battery_error_detected = None
-        self.battery_above_voltage_threshold = None
-        
+
+        self.powered_by_grid: Optional[bool] = None
+        self.battery_is_fully_charged: Optional[bool] = None
+        self.battery_error_detected: Optional[bool] = None
+        self.battery_above_voltage_threshold: Optional[bool] = None
 
         # use underlying pigpio library
         self.pin_factory = utils.get_gpio_pin_factory()
 
         self.logger.info("Finished initialization")
-    
-    def _read_power_mode(self):
+
+    def _read_power_mode(self) -> None:
         """
         UPS_BATTERY_MODE_PIN_IN is HIGH when the system is powered by the battery
         UPS_BATTERY_MODE_PIN_IN is LOW when the system is powered by the grid
         """
-        
+
         power_mode = gpiozero.DigitalInputDevice(
             UPS_BATTERY_MODE_PIN_IN,
             bounce_time=0.3,
             pin_factory=self.pin_factory,
         )
-        
+
         if not power_mode.is_active:
             self.logger.info("system is powered by the grid")
             self.powered_by_grid = True
         else:
             self.logger.info("system is powered by the battery")
             self.powered_by_grid = False
-        
-    
-    def _read_battery_state(self):
+
+    def _read_battery_state(self) -> None:
         """
         UPS_STATUS_READY is HIGH when the battery is fully charged
         UPS_STATUS_READY is LOW when the battery is not fully charged
         (UPS_STATUS_READY is HIGH & UPS_BATTERY_MODE_PIN_IN is HIGH) when the system is powered by the system and the battery voltage has dropped to a minimum
         """
-        
+
         battery_state = gpiozero.DigitalInputDevice(
             UPS_READY_PIN_IN, bounce_time=0.3, pin_factory=self.pin_factory
         )
-        
+
         power_mode = gpiozero.DigitalInputDevice(
             UPS_BATTERY_MODE_PIN_IN,
             bounce_time=0.3,
             pin_factory=self.pin_factory,
         )
-        
+
         if battery_state.is_active:
             self.logger.info("the battery is fully charged")
             self.battery_is_fully_charged = True
         else:
             self.logger.info("the battery is not fully charged")
             self.battery_is_fully_charged = False
-            
-            
+
         # this is probably never reached as the power is shut down in this case
-        if (battery_state.is_active & (power_mode.is_active)):
-            self.logger.info("the battery voltage has dropped below the minimum threshold")
+        if battery_state.is_active & (power_mode.is_active):
+            self.logger.info(
+                "the battery voltage has dropped below the minimum threshold"
+            )
             self.battery_above_voltage_threshold = False
         else:
             self.logger.info("the battery voltage is above the minimum threshold")
             self.battery_above_voltage_threshold = True
-            
-    
-    def _read_alarm_state(self):
+
+    def _read_alarm_state(self) -> None:
         """
         UPS_ALARM_PIN_IN is HIGH when a battery error is detected
         UPS_ALARM_PIN_IN is LOW when the battery status is okay
         """
-        
+
         alarm_state = gpiozero.DigitalInputDevice(
             UPS_ALARM_PIN_IN, bounce_time=0.3, pin_factory=self.pin_factory
         )
-        
+
         if not alarm_state.is_active:
             self.logger.info("the battery status is fine")
             self.battery_error_detected = False
         else:
             self.logger.info("a battery error was detected")
             self.battery_error_detected = True
-            
-    def update_ups_status(self):
-        
+
+    def update_ups_status(self) -> None:
         self._read_power_mode()
         self._read_battery_state()
         self._read_alarm_state()
