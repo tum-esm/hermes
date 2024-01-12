96566b92157734d54c6c46ef4cd7d3a3c660b313
Patrick <patrick.aigner@tum.de>
2024-01-12 10:07:22 +0100

    round wind measurement to 1 digit

--- sensor/src/utils/functions.py
+++ sensor/src/utils/functions.py
@@ -137,6 +137,7 @@ def set_alarm(timeout: int, label: str) -> None:
     signal.alarm(timeout)
 
 
-def avg_list(input_list: list[float]) -> float:
-    """averages a list of float"""
-    return sum(input_list) / len(input_list)
+def avg_list(input_list: list[float], round_digits: int = 2) -> float:
+    """Averages a list of float.
+    Returns a float rounded to defined digits."""
+    return round(sum(input_list) / len(input_list), round_digits)
