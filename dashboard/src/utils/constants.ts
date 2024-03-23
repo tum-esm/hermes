a753983ba9154285e4bb048e1b87fb0c11f224a9
Lars Frölich <lars.froelich@gmail.com>
2024-03-23 13:31:05 +0100

    added error alert if server-url env-var is not defined

--- dashboard/src/utils/constants.ts
+++ dashboard/src/utils/constants.ts
@@ -1,1 +1,7 @@
+
+if (!import.meta.env.VITE_SERVER_URL) {
+  alert('vite-error: the environment variable VITE_SERVER_URL is required during build!');
+  throw new Error('VITE_SERVER_URL is required');
+}
+
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
