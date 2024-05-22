980a7ec1762e0b0f507b4ace0f5cdf195548cfb6
Patrick <patrick.aigner@tum.de>
2024-05-22 14:26:41 +0200

    add new ups test #151

---
+++ sensor/tests/integration/test_ups.py
@@ -0,0 +1,21 @@
+from os.path import dirname, abspath
+import sys
+import pytest
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.append(PROJECT_DIR)
+from src import utils, hardware
+
+
+@pytest.mark.integration
+def test_ups() -> None:
+    config = utils.ConfigInterface.read()
+    ups = hardware.UPSInterface(config)
+    ups.update_ups_status()
+
+    assert ups.powered_by_grid is not None
+    assert ups.battery_is_fully_charged is not None
+    assert ups.battery_error_detected is not None
+    assert ups.battery_above_voltage_threshold is not None
+
+    ups.teardown()
