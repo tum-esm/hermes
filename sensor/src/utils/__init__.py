e0c6ba80aae00fa73b7dc834f44eabe4f9f897b8
Patrick <patrick.aigner@tum.de>
2024-05-22 11:35:35 +0200

    add read_os_uptime to init #187

--- sensor/src/utils/__init__.py
+++ sensor/src/utils/__init__.py
@@ -10,6 +10,7 @@ from .functions import (
     get_cpu_temperature,
     set_alarm,
     ExponentialBackOff,
+    read_os_uptime,
 )
 from .logger import Logger
 from .message_queue import MessageQueue
