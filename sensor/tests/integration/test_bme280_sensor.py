7a8a33bfd78537e4321471a2c9c5e639ab4fb65a
Patrick <patrick.aigner@tum.de>
2024-05-22 13:24:27 +0200

    update BME280 test #151

--- sensor/tests/integration/test_bme280_sensor.py
+++ sensor/tests/integration/test_bme280_sensor.py
@@ -9,13 +9,20 @@ from src import hardware, utils
 
 @pytest.mark.integration
 def test_bme280_sensor(log_files: None) -> None:
+    """Two BME280 sensors are present in the system.
+    BME280 sensor measure temperature, humidity and pressure."""
+
     config = utils.ConfigInterface.read()
-    s1 = hardware.BME280SensorInterface(config, variant="mainboard")
+    s1 = hardware.BME280SensorInterface(config, variant="ioboard")
     data1 = s1.get_data()
     assert data1.temperature is not None
+    assert data1.humidity is not None
+    assert data1.pressure is not None
     s1.teardown()
 
     s2 = hardware.BME280SensorInterface(config, variant="air-inlet")
     data2 = s2.get_data()
     assert data2.temperature is not None
+    assert data2.humidity is not None
+    assert data2.pressure is not None
     s2.teardown()
