2bc1fc6b3110409bf3f174b1e7580add6d498561
Patrick <patrick.aigner@tum.de>
2024-05-23 16:39:32 +0200

    readd config fixture

--- sensor/tests/conftest.py
+++ sensor/tests/conftest.py
@@ -1,12 +1,13 @@
-import os
 from datetime import datetime
-import dotenv
-from typing import Any, Optional
+import json
+import os
 import random
-import pytest
 import time
+from typing import Any, Optional
+import pytest
+import dotenv
 from os.path import dirname, abspath, join, isfile
-from src import utils, procedures
+from src import custom_types, utils, procedures
 
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 
@@ -48,6 +49,71 @@ def _restore_file(original_path: str, temporary_path: str) -> None:
 
 
 @pytest.fixture(scope="function")
+def sample_config() -> Any:
+    """use the config template as a temporary config file"""
+    CONFIG_FILE = join(PROJECT_DIR, "config", "config.json")
+    TMP_CONFIG_FILE = join(PROJECT_DIR, "config", "config.tmp.json")
+
+    with open(join(PROJECT_DIR, "config", "config.template.json")) as f:
+        sample_config = custom_types.Config(**json.load(f))
+
+    _save_file(
+        CONFIG_FILE,
+        TMP_CONFIG_FILE,
+        json.dumps(sample_config.dict(), indent=4),
+    )
+
+    yield sample_config
+
+    _restore_file(CONFIG_FILE, TMP_CONFIG_FILE)
+
+
+@pytest.fixture(scope="function")
+def log_files() -> Any:
+    """
+    1. store actual log files in a temporary location
+    2. set up a new, empty log file just for the test
+    3. restore the original log file after the test
+    """
+
+    LOG_FILE = join(
+        PROJECT_DIR, "logs", "archive", datetime.now().strftime("%Y-%m-%d.log")
+    )
+    TMP_LOG_FILE = join(
+        PROJECT_DIR, "logs", "archive", datetime.now().strftime("%Y-%m-%d.tmp.log")
+    )
+
+    _save_file(LOG_FILE, TMP_LOG_FILE, "")
+
+    yield
+
+    _restore_file(LOG_FILE, TMP_LOG_FILE)
+
+    with open(TMP_LOG_FILE) as f:
+        logs_after_test = f.read()
+    print("*** LOGS AFTER TEST:")
+    print(logs_after_test, end="")
+    print("***")
+
+
+@pytest.fixture(scope="function")
+def mqtt_client_environment() -> Any:
+    """load the environment variables from config/.env.testing
+    and generate a dummy base-topic path"""
+
+    dotenv.load_dotenv(join(PROJECT_DIR, "config", ".env"))
+    dotenv.load_dotenv(join(PROJECT_DIR, "config", ".env.testing"))
+
+    timestamp = round(time.time())
+    os.environ["HERMES_MQTT_IDENTIFIER"] = "".join(
+        random.choices([chr(a) for a in range(ord("a"), ord("z") + 1)], k=20)
+    )
+    os.environ["HERMES_MQTT_BASE_TOPIC"] = f"development/test-{timestamp}/"
+
+    yield
+
+
+@pytest.fixture(scope="function")
 def mqtt_data_files() -> Any:
     """start and stop the background sending loop of the SendingMQTTClient"""
     print("clearing mqtt data files")
@@ -76,23 +142,6 @@ def mqtt_data_files() -> Any:
 
     _restore_file(ACTIVE_MESSAGES_FILE, TMP_ACTIVE_MESSAGES_FILE)
     _restore_file(MESSAGE_ARCHIVE_FILE, TMP_MESSAGE_ARCHIVE_FILE)
-
-
-@pytest.fixture(scope="function")
-def mqtt_client_environment() -> Any:
-    """load the environment variables from config/.env.testing
-    and generate a dummy base-topic path"""
-
-    dotenv.load_dotenv(join(PROJECT_DIR, "config", ".env"))
-    dotenv.load_dotenv(join(PROJECT_DIR, "config", ".env.testing"))
-
-    timestamp = round(time.time())
-    os.environ["HERMES_MQTT_IDENTIFIER"] = "".join(
-        random.choices([chr(a) for a in range(ord("a"), ord("z") + 1)], k=20)
-    )
-    os.environ["HERMES_MQTT_BASE_TOPIC"] = f"development/test-{timestamp}/"
-
-    yield
 
 
 @pytest.fixture(scope="function")
