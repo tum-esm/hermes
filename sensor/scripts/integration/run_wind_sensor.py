33cffa4561dc1bff067f0242ec45d3157d30d7ce
Patrick <patrick.aigner@tum.de>
2024-05-23 15:04:26 +0200

    fix typing checks

--- sensor/scripts/integration/run_wind_sensor.py
+++ sensor/scripts/integration/run_wind_sensor.py
@@ -11,7 +11,5 @@ except Exception as e:
 wind_sensor = hardware.WindSensorInterface(config, testing=True)
 
 while True:
-    print(wind_sensor.get_current_device_status())
-    time.sleep(1)
-    print(wind_sensor.get_current_wind_measurement())
+    print(wind_sensor.get_current_sensor_measurement())
     time.sleep(1)
