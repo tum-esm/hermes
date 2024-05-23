6f45cd50d9e44b6c0746d37f95cc190b9defff41
Patrick <patrick.aigner@tum.de>
2024-05-23 14:52:52 +0200

    refactor wind sensor test #151

--- sensor/tests/integration/test_wxt532_sensor.py
+++ sensor/tests/integration/test_wxt532_sensor.py
@@ -5,21 +5,11 @@ from src import utils, hardware
 
 @pytest.mark.integration
 def test_wind_sensor() -> None:
+    """This tests if the current configuration with or without a
+    wind sensor present will cause any problems."""
     config = utils.ConfigInterface.read()
     wind_sensor = hardware.WindSensorInterface(config)
 
-    def data_arrived() -> bool:
-        d1, d2 = wind_sensor.get_current_sensor_measurement()
-
-        print(f"measurement: {d1}")
-        print(f"device_status: {d2}")
-        return d1 is not None
-
-    wait_for_condition(
-        data_arrived,
-        timeout_message="wind sensor did not send data",
-        timeout_seconds=30,
-    )
-
-    print("tearing down interface")
+    wind_sensor.get_current_sensor_measurement()
+    wind_sensor.check_errors()
     wind_sensor.teardown()
